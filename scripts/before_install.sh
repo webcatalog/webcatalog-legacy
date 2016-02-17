29ef22c91a918bcb8656be429e85b28841562850
Quang Lam <quang.lam2807@gmail.com>
2016-02-17 13:30:17 +0100

    Use Linux to build Linux & Windows

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,26 +1,37 @@
-brew update
-
-brew install hugo
-
-brew upgrade node
-node -v
-npm install -g nativefier
-npm install -g electron-builder
-npm install -g surge
-
-
-if [ "$BUILD_TYPE" = "windows" ]
+if [ "$TRAVIS_OS_NAME" == "osx" ]
 then
-  brew install makensis
-  brew install wine
-fi
+  brew update
 
-if [ "$BUILD_TYPE" = "osx" ]
-then
+  brew install hugo
   brew install imagemagick
+
+  brew upgrade node
+  node -v
+  npm install -g nativefier
+  npm install -g electron-builder
+  npm install -g surge
 fi
 
-if [ "$BUILD_TYPE" = "linux" ]
+if [ "$TRAVIS_OS_NAME" == "linux" ]
 then
-  gem install fpm
+  if [ "$BUILD_TYPE" = "windows" ]
+  then
+    sudo add-apt-repository ppa:ubuntu-wine/ppa -y
+    sudo apt-get update
+    sudo apt-get install wine nsis -y
+  fi
+
+  if [ "$BUILD_TYPE" = "linux" ]
+  then
+    gem install fpm
+  fi
+
+  sudo apt-get install imagemagick
+  go get -v github.com/spf13/hugo
+
+
+  node -v
+  npm install -g nativefier
+  npm install -g electron-builder
+  npm install -g surge
 fi
