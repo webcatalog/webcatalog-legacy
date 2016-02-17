3704ad23adf48314cc5bb2a98d7cdaec67c3999b
Quang Lam <quang.lam2807@gmail.com>
2016-02-17 22:22:14 +0100

    Simplify after_deploy

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -1,8 +1,7 @@
 set -e
 
-echo leader=$BUILD_LEADER status=$BUILD_AGGREGATE_STATUS
-if [ "$BUILD_LEADER" = "YES" ]; then
-  if [ "$BUILD_AGGREGATE_STATUS" = "others_succeeded" ]; then
-    $(npm bin)/surge --project ./public --domain webcatalog.xyz
-  fi
+if [ "$TRAVIS_OS_NAME" == "osx" ]
+then
+  hugo
+  $(npm bin)/surge --project ./public --domain webcatalog.xyz
 fi
