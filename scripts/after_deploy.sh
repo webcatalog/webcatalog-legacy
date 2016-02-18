a3dfc31cc02dd4d673d8939dae511ce097687fe1
Quang Lam <quang.lam2807@gmail.com>
2016-02-18 22:59:53 +0100

    Fix update_algolia_data.js path

--- scripts/after_deploy.sh
+++ scripts/after_deploy.sh
@@ -3,5 +3,5 @@ set -e
 if [ "$TRAVIS_OS_NAME" == "osx" ]
 then
   $(npm bin)/surge --project ./public --domain webcatalog.xyz
-  node ./update_algolia_data.js
+  node ./scripts/update_algolia_data.js
 fi
