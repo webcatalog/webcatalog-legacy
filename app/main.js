2fe27b5043c8035ddc2426bb1bcf0baacc867e86
Quang Lam <quang.lam2807@gmail.com>
2017-01-09 23:55:01 -0600

    Add back electron-auto-updater

--- app/main.js
+++ app/main.js
@@ -224,32 +224,57 @@ function createWindow() {
   mainWindow.webContents.once('did-finish-load', () => {
     setTimeout(() => {
       // Auto updater
-      /* global fetch */
-      https.get('https://backend.getwebcatalog.com/latest.json', (res) => {
-        let body = '';
-        res.on('data', (chunk) => {
-          body += chunk;
+      if (process.platform === 'darwin') {
+        https.get('https://backend.getwebcatalog.com/latest.json', (res) => {
+          let body = '';
+          res.on('data', (chunk) => {
+            body += chunk;
+          });
+          res.on('end', () => {
+            const latestVersion = JSON.parse(body).version;
+            log(`Lastest version ${latestVersion}`);
+            if (semver.gt(latestVersion, app.getVersion())) {
+              dialog.showMessageBox(mainWindow, {
+                type: 'info',
+                buttons: ['Yes', 'Cancel'],
+                defaultId: 1,
+                title: 'A new update is ready to install',
+                message: `WebCatalog ${latestVersion} is now available. Do you want to go to the website and download now?`,
+              }, (response) => {
+                if (response === 0) {
+                  shell.openExternal('https://getwebcatalog.com');
+                }
+              });
+            }
+          });
+        }).on('error', (err) => {
+          log(`Update checker: ${err.message}`);
         });
-        res.on('end', () => {
-          const latestVersion = JSON.parse(body).version;
-          log(`Lastest version ${latestVersion}`);
-          if (semver.gt(latestVersion, app.getVersion())) {
-            dialog.showMessageBox(mainWindow, {
-              type: 'info',
-              buttons: ['Yes', 'Cancel'],
-              defaultId: 1,
-              title: 'A new update is ready to install',
-              message: `WebCatalog ${latestVersion} is now available. Do you want to go to the website and download now?`,
-            }, (response) => {
-              if (response === 0) {
-                shell.openExternal('https://getwebcatalog.com');
-              }
-            });
-          }
+      } else {
+        /* eslint-disable global-require */
+        const autoUpdater = require('electron-auto-updater').autoUpdater;
+        /* eslint-enable global-require */
+        autoUpdater.addListener('update-downloaded', (event, releaseNotes, releaseName) => {
+          dialog.showMessageBox({
+            type: 'info',
+            buttons: ['Yes', 'Cancel'],
+            defaultId: 1,
+            title: 'A new update is ready to install',
+            message: `Version ${releaseName} is downloaded and will be automatically installed. Do you want to quit the app to install it now?`,
+          }, (response) => {
+            if (response === 0) {
+              autoUpdater.quitAndInstall();
+            }
+          });
         });
-      }).on('error', (err) => {
-        log(`Update checker: ${err.message}`);
-      });
+
+        autoUpdater.addListener('error', err => log(`Update error: ${err.message}`));
+        autoUpdater.on('checking-for-update', () => log('Checking for update'));
+        autoUpdater.on('update-available', () => log('Update available'));
+        autoUpdater.on('update-not-available', () => log('No update available'));
+
+        autoUpdater.checkForUpdates();
+      }
     }, 1000);
   });
 }
