ceed5ad744b723365f6ace6f8333bded590f96ab
Quang Lam <quang.lam2807@gmail.com>
2017-01-24 21:06:52 -0600

    2.8.2

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const appPageTargetPath = `${targetPath}/apps/page`;
 const numberOfAppInChunk = 24;
 
 // released version
-const latestVersion = '2.8.1';
+const latestVersion = '2.8.2';
 
 // init target folders
 mkdirp.sync(imageTargetPath);
