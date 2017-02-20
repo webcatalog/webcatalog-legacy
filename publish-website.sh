35df1f2179d4d980916358b3bd7cb16968cb5c0b
Quang Lam <quang.lam2807@gmail.com>
2017-02-20 09:36:36 -0600

    GitHub Pages

--- publish-website.sh
+++ publish-website.sh
@@ -3,10 +3,17 @@
 if [ "$TRAVIS_OS_NAME" == 'osx' -a "$TRAVIS_BRANCH" = "master" -a -n "TRAVIS_TAG" ]; then
   echo "Publishing Website"
   export WEBCATALOG_VERSION=$(node -e "console.log(require('./app/package.json').version);")
-  yarn global add harp surge
+  yarn global add harp@0.3.1
   cd website
   yarn
   mv ./node_modules ./_node_modules
   harp compile
-  surge --project ./www --domain getwebcatalog.com
+
+  cd www
+  git init
+  git checkout -b gh-pages
+  git add .
+  git -c user.name='Quang Lam' -c user.email='quang.lam2807@gmail.com' commit -m init
+  git remote add origin "https://$GH_TOKEN@github.com/webcatalog/desktop.git"
+  git push origin gh-pages -f
 fi
