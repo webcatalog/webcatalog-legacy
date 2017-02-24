ecc86c61c68812a929c090ff747d57d71985a489
Quang Lam <quang.lam2807@gmail.com>
2017-02-24 12:16:07 -0600

    Fix website publishing script

--- publish-website.sh
+++ publish-website.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_OS_NAME" == 'osx' -a "$TRAVIS_BRANCH" = "master" -a -n "TRAVIS_TAG" ]; then
+if [ "$TRAVIS_OS_NAME" == 'linux' -a "$TRAVIS_BRANCH" = "master" -a -n "TRAVIS_TAG" ]; then
   echo "Publishing Website"
   export WEBCATALOG_VERSION=$(node -e "console.log(require('./app/package.json').version);")
   yarn global add harp@0.3.1
@@ -16,4 +16,8 @@ if [ "$TRAVIS_OS_NAME" == 'osx' -a "$TRAVIS_BRANCH" = "master" -a -n "TRAVIS_TAG
   git -c user.name='Quang Lam' -c user.email='quang.lam2807@gmail.com' commit -m init
   git remote add origin "https://$GH_TOKEN@github.com/webcatalog/desktop.git"
   git push origin gh-pages -f
+else
+  echo $TRAVIS_OS_NAME
+  echo $TRAVIS_BRANCH
+  echo "Skip Publishing Website"
 fi
