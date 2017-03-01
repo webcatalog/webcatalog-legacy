5440cc606c4618256aa67ab74993cc68f0e95010
Quang Lam <quang.lam2807@gmail.com>
2017-03-01 13:05:42 -0600

    Add Travis Job Number for gh-pages commit name

--- publish-website.sh
+++ publish-website.sh
@@ -13,7 +13,7 @@ if [ "$TRAVIS_OS_NAME" == 'linux' -a "$TRAVIS_BRANCH" = "master" ]; then
   git init
   git checkout -b gh-pages
   git add .
-  git -c user.name='Quang Lam' -c user.email='quang.lam2807@gmail.com' commit -m init
+  git -c user.name='Quang Lam' -c user.email='quang.lam2807@gmail.com' commit -m "${TRAVIS_JOB_NUMBER}"
   git remote add origin "https://$GH_TOKEN@github.com/webcatalog/desktop.git"
   git push origin gh-pages -f --quiet > /dev/null 2>&1
 else
