83884e8c0c175b8c9050af84a77d1b97644cd3f7
Quang Lam <quang.lam2807@gmail.com>
2017-03-11 15:46:12 -0600

    Use compiled instead of gh-pages. Prepare to remove GH Pages

--- publish.sh
+++ publish.sh
@@ -2,8 +2,8 @@
 
 cd www
 git init
-git checkout -b gh-pages
+git checkout -b compiled
 git add .
 git -c user.name='Quang Lam' -c user.email='quang.lam2807@gmail.com' commit -m "${TRAVIS_JOB_NUMBER}"
 git remote add origin "https://$GH_TOKEN@github.com/webcatalog/backend.git"
-git push origin gh-pages -f
+git push origin compiled -f
