7e54ce2f0d5d5a4b5f48d5967e29b17a46d6d3c8
Quang Lam <quang.lam2807@gmail.com>
2017-03-15 13:02:15 -0500

    Use system Flash

--- app/libs/loadPlugins.js
+++ app/libs/loadPlugins.js
@@ -3,22 +3,7 @@
 const { app } = require('electron');
 const path = require('path');
 
-const FLASH_VERSION = '24.0.0.194';
 const WIDEVINECDM_VERSION = '1.4.8.962';
-
-let flashPluginFilename;
-switch (process.platform) {
-  case 'darwin':
-    flashPluginFilename = 'PepperFlashPlayer.plugin';
-    break;
-  case 'linux':
-    flashPluginFilename = 'libpepflashplayer.so';
-    break;
-  default:
-  case 'win32':
-    flashPluginFilename = 'pepflashplayer.dll';
-    break;
-}
 
 let widevineCdmPluginFilename;
 switch (process.platform) {
@@ -37,8 +22,7 @@ const pluginFolder = `plugins/${process.platform}/${process.arch}`;
 
 const loadPlugins = () => {
   // load plugins
-  app.commandLine.appendSwitch('ppapi-flash-path', path.join(__dirname, '..', pluginFolder, 'PepperFlash', FLASH_VERSION, flashPluginFilename).replace('app.asar', 'app.asar.unpacked'));
-  app.commandLine.appendSwitch('ppapi-flash-version', FLASH_VERSION);
+  app.commandLine.appendSwitch('ppapi-flash-path', app.getPath('pepperFlashSystemPlugin'));
 
   app.commandLine.appendSwitch('widevine-cdm-path', path.join(__dirname, '..', pluginFolder, 'WidevineCdm', WIDEVINECDM_VERSION, widevineCdmPluginFilename).replace('app.asar', 'app.asar.unpacked'));
   app.commandLine.appendSwitch('widevine-cdm-version', WIDEVINECDM_VERSION);
