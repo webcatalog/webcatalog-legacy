37a2122b3ff884fca05614702373838320ff00e2
Quang Lam <quang.lam2807@gmail.com>
2017-03-15 13:23:18 -0500

    Remove WidevineCDM

--- app/libs/loadPlugins.js
+++ app/libs/loadPlugins.js
@@ -1,31 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
 const { app } = require('electron');
-const path = require('path');
-
-const WIDEVINECDM_VERSION = '1.4.8.962';
-
-let widevineCdmPluginFilename;
-switch (process.platform) {
-  case 'darwin':
-    widevineCdmPluginFilename = 'widevinecdmadapter.plugin';
-    break;
-  case 'linux':
-    widevineCdmPluginFilename = 'libwidevinecdmadapter.so';
-    break;
-  default:
-  case 'win32':
-    widevineCdmPluginFilename = 'widevinecdmadapter.dll';
-}
-
-const pluginFolder = `plugins/${process.platform}/${process.arch}`;
 
 const loadPlugins = () => {
   // load plugins
   app.commandLine.appendSwitch('ppapi-flash-path', app.getPath('pepperFlashSystemPlugin'));
-
-  app.commandLine.appendSwitch('widevine-cdm-path', path.join(__dirname, '..', pluginFolder, 'WidevineCdm', WIDEVINECDM_VERSION, widevineCdmPluginFilename).replace('app.asar', 'app.asar.unpacked'));
-  app.commandLine.appendSwitch('widevine-cdm-version', WIDEVINECDM_VERSION);
 };
 
 module.exports = loadPlugins;
