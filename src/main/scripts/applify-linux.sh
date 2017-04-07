b3d00ae9dd8ccc3cc95dbbc8156cca3df6677a0e
Quang Lam <quang.lam2807@gmail.com>
2017-04-07 14:26:01 -0500

    Update Linux installation script

--- src/main/scripts/applify-linux.sh
+++ src/main/scripts/applify-linux.sh
@@ -7,23 +7,35 @@ APPURL=${2}
 APPICON=${3}
 APPID=${4}
 JSONCONTENT=${5}
-APPDIR=$(eval echo "~/.local/share/applications")
 ICONDIR=$(eval echo "~/.icons/webcatalog")
-DESKTOP_FILE="${APPDIR}/${APPID}.desktop"
+
+BINDIR="/usr/local/bin/webcatalog"
+BINFILE="/usr/local/bin/${APPID}"
+
+APPDIR=$(eval echo "~/.local/share/applications")
+DESKTOPFILE="${APPDIR}/${APPID}.desktop"
 
 echo $DESKTOP_FILE
+
+mkdir -p "${BINDIR}"
 
 mkdir -p "${APPDIR}"
 
 mkdir -p "${ICONDIR}"
 
+cat <<EOF > "${BINFILE}"
+#!/usr/bin/env bash
+/usr/local/bin/webcatalog --name="$APPNAME" --url="$APPURL" --id="$APPID"
+EOF
+chmod +x "${BINFILE}"
+
 cp -v "$APPICON" "${ICONDIR}/${APPID}.png"
 
-cat <<EOF > "${DESKTOP_FILE}"
+cat <<EOF > "${DESKTOPFILE}"
 [Desktop Entry]
 #${JSONCONTENT}
 Name=$APPNAME
-Exec=/usr/local/bin/webcatalog --name="$APPNAME" --url="$APPURL" --id="$APPID"
+Exec=$BINFILE
 Icon=$ICONDIR/$APPID.png
 Type=Application
 EOF
