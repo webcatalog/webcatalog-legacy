073ac01af763d0b1d5a42fba41dfa81798957287
Quang Lam <quang.lam2807@gmail.com>
2017-04-12 08:04:13 -0500

    Remove electron-settings Promise syntax

--- src/main/preload.js
+++ src/main/preload.js
@@ -9,34 +9,30 @@ const argv = remote.getCurrentWindow().appInfo;
 
 window.onload = () => {
   // inject JS
-  settings.get(`behaviors.${camelCase(argv.id)}.injectedJS`, null)
-    .then((injectedJS) => {
-      if (!injectedJS || injectedJS.trim().length < 1) return;
+  const injectedJS = settings.get(`behaviors.${camelCase(argv.id)}.injectedJS`, null);
+  if (!injectedJS || injectedJS.trim().length < 1) return;
 
-      try {
-        const node = document.createElement('script');
-        node.innerHTML = injectedJS;
-        document.body.appendChild(node);
-      } catch (err) {
-        /* eslint-disable no-console */
-        console.log(err);
-        /* eslint-enable no-console */
-      }
-    });
+  try {
+    const node = document.createElement('script');
+    node.innerHTML = injectedJS;
+    document.body.appendChild(node);
+  } catch (err) {
+    /* eslint-disable no-console */
+    console.log(err);
+    /* eslint-enable no-console */
+  }
 
   // inject CSS
-  settings.get(`behaviors.${camelCase(argv.id)}.injectedCSS`, null)
-    .then((injectedCSS) => {
-      if (!injectedCSS || injectedCSS.trim().length < 1) return;
+  const injectedCSS = settings.get(`behaviors.${camelCase(argv.id)}.injectedCSS`, null);
+  if (!injectedCSS || injectedCSS.trim().length < 1) return;
 
-      try {
-        const node = document.createElement('style');
-        node.innerHTML = injectedCSS;
-        document.body.appendChild(node);
-      } catch (err) {
-        /* eslint-disable no-console */
-        console.log(err);
-        /* eslint-enable no-console */
-      }
-    });
+  try {
+    const node = document.createElement('style');
+    node.innerHTML = injectedCSS;
+    document.body.appendChild(node);
+  } catch (err) {
+    /* eslint-disable no-console */
+    console.log(err);
+    /* eslint-enable no-console */
+  }
 };
