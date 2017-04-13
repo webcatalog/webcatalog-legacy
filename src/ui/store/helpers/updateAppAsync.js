223a6a621e377218513be901f3552b489a6f4e7f
Quang Lam <quang.lam2807@gmail.com>
2017-04-12 22:09:24 -0500

    Improve update compatibility with 3.x

--- src/ui/store/helpers/updateAppAsync.js
+++ src/ui/store/helpers/updateAppAsync.js
@@ -1,8 +1,21 @@
+/* global fetch */
 import installAppAsync from '../helpers/installAppAsync';
 import uninstallAppAsync from '../helpers/uninstallAppAsync';
 
 const updateAppAsync = ({ allAppPath, appId, appName, appUrl }) =>
   uninstallAppAsync({ allAppPath, appId, appName, shouldClearStorageData: false })
-    .then(() => installAppAsync({ appId, appName, appUrl }));
+    .then(() => {
+      // if missing appName or appUrl, get them from server
+      if (!appName || !appUrl) {
+        return fetch(`https://raw.githubusercontent.com/webcatalog/webcatalog-backend/master/data/json/${appId}.json`)
+          .then(response => response.json());
+      }
+
+      return {
+        name: appName,
+        url: appUrl,
+      };
+    })
+    .then(appInfo => installAppAsync({ appId, appName: appInfo.name, appUrl: appInfo.url }));
 
 export default updateAppAsync;
