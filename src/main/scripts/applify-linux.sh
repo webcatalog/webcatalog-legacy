8de789def93aa14cec9da0b9f9a0a4f6c3943768
Quang Lam <quang.lam2807@gmail.com>
2017-04-13 12:19:41 -0500

    Fall back to old linux applify

--- src/main/scripts/applify-linux.sh
+++ src/main/scripts/applify-linux.sh
@@ -7,35 +7,23 @@ APPURL=${2}
 APPICON=${3}
 APPID=${4}
 JSONCONTENT=${5}
-ICONDIR=$(eval echo "~/.icons/webcatalog")
-
-BINDIR=$(eval echo "~/.local/share/webcatalog")
-BINFILE="${BINDIR}/${APPID}"
-
 APPDIR=$(eval echo "~/.local/share/applications")
-DESKTOPFILE="${APPDIR}/webcatalog-${APPID}.desktop"
+ICONDIR=$(eval echo "~/.icons/webcatalog")
+DESKTOP_FILE="${APPDIR}/webcatalog-${APPID}.desktop"
 
 echo $DESKTOP_FILE
-
-mkdir -p "${BINDIR}"
 
 mkdir -p "${APPDIR}"
 
 mkdir -p "${ICONDIR}"
 
-cat <<EOF > "${BINFILE}"
-#!/usr/bin/env bash
-/opt/WebCatalog/webcatalog --name="$APPNAME" --url="$APPURL" --id="$APPID"
-EOF
-chmod +x "${BINFILE}"
-
 cp -v "$APPICON" "${ICONDIR}/${APPID}.png"
 
-cat <<EOF > "${DESKTOPFILE}"
+cat <<EOF > "${DESKTOP_FILE}"
 [Desktop Entry]
 #${JSONCONTENT}
 Name=$APPNAME
-Exec=$BINFILE
+Exec=/usr/local/bin/webcatalog --name="$APPNAME" --url="$APPURL" --id="$APPID"
 Icon=$ICONDIR/$APPID.png
 Type=Application
 EOF
