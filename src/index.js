72f482af6f26ea096afcfd64384044e1f66b6cff
Quang Lam <quanglam2807@Quangs-MacBook-Pro.local>
2017-04-21 22:58:03 -0500

    Initial

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000!');
+});
