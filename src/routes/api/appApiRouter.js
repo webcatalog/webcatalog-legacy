f51c917bab063c8d499d398b366efb55f1c86ae0
Quang Lam <quang.lam2807@gmail.com>
2017-04-29 21:30:32 -0500

    Update Api

--- src/routes/api/appApiRouter.js
+++ src/routes/api/appApiRouter.js
@@ -6,8 +6,17 @@ const appApiRouter = express.Router();
 
 appApiRouter.get('/', (req, res, next) => {
   App.findAll({ where: { isActive: true } })
-    .then((apps) => {
-      res.json(apps);
+    .then(apps => res.json(apps))
+    .catch((err) => {
+      next(err);
+    });
+});
+
+appApiRouter.get('/:id', (req, res, next) => {
+  App.find({ where: { id: req.params.id, isActive: true } })
+    .then((app) => {
+      if (!app) throw new Error('App not found');
+      return res.json(app);
     })
     .catch((err) => {
       next(err);
