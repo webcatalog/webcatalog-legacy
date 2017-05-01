e4d77d5dbd0bc7d62aff6c7073f87abc44f31d5f
Quang Lam <quang.lam2807@gmail.com>
2017-05-01 14:41:43 -0500

    Improve download routes speed

--- src/routes/downloads/index.js
+++ src/routes/downloads/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 
-import App from '../../models/App';
-
 const downloadsRouter = express.Router();
 
 downloadsRouter.get(['/', '/downloads'], (req, res) => {
@@ -15,7 +13,7 @@ downloadsRouter.get(['/', '/downloads'], (req, res) => {
   }
 });
 
-downloadsRouter.get('/downloads/:platform(mac|windows|linux)', (req, res, next) => {
+downloadsRouter.get('/downloads/:platform(mac|windows|linux)', (req, res) => {
   const platform = req.params.platform;
   const platformName = platform.charAt(0).toUpperCase() + platform.slice(1);
 
@@ -23,30 +21,12 @@ downloadsRouter.get('/downloads/:platform(mac|windows|linux)', (req, res, next) 
   if (platform === 'windows') dockName = 'taskbar';
   if (platform === 'linux') dockName = 'launcher';
 
-  App.findAll({
-    where: {
-      id: {
-        $in: [
-          '1eceee7d-fb6c-49ff-ac65-d698842f7135',
-          'a2c752cf-ea98-4434-88ea-c23b0cdec645',
-          '621434c9-8f4d-4c06-83fa-9b3b99e3820b',
-          '69b548b4-1128-4783-b727-453a9358c210',
-          '43c2935a-77d9-4e9d-88cd-c603f7bce926',
-
-          '867609d2-6037-441f-a425-79a7032da1fa',
-          'c7b88878-7b4d-406e-9a9b-433bddb0b071',
-          'f2374bfb-ddc8-4d54-82c8-e3e235528626',
-          '0bbee208-4381-4f7b-90fa-ad752d23a4da',
-          'e72e14f4-d97f-4b5e-be0e-af7aa63cb1f5',
-        ],
-      },
-      isActive: true,
-    },
-    attributes: ['id', 'name', 'url', 'slug'],
-    order: [['installCount', 'DESC']],
-  })
-  .then(topApps => res.render('downloads/index', { version: process.env.VERSION, platform, dockName, topApps, title: `Download WebCatalog for ${platformName}` }))
-  .catch(next);
+  res.render('downloads/index', {
+    version: process.env.VERSION,
+    platform,
+    dockName,
+    title: `Download WebCatalog for ${platformName}`,
+  });
 });
 
 module.exports = downloadsRouter;
