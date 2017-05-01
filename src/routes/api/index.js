8827f2c9bc01a6dbc41796a1be71be858383fffd
Quang Lam <quang.lam2807@gmail.com>
2017-05-01 17:23:22 -0500

    Add user api route

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import passport from 'passport';
+import crypto from 'crypto';
 
 import appApi from './appApiRouter';
 
 const apiRouter = express.Router();
 
 apiRouter.use('/apps', appApi);
+
+apiRouter.get('/user', passport.authenticate('jwt', { session: false }), (req, res) => {
+  const hmac = crypto.createHmac('sha256', process.env.INTERCOM_SECRET);
+  hmac.update(req.user.id);
+
+  return res.json({
+    user: {
+      id: req.user.id,
+      email: req.user.email,
+      displayName: req.user.displayName,
+      profilePicture: req.user.profilePicture,
+      intercomUserHash: hmac.digest('hex'),
+    },
+  });
+});
 
 module.exports = apiRouter;
