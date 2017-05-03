a80277b93bf24ecd44f50e7b0e11fa08624fe7a7
Quang Lam <quang.lam2807@gmail.com>
2017-05-03 00:09:42 -0500

    Fix content type

--- src/routes/s3/index.js
+++ src/routes/s3/index.js
@@ -9,12 +9,41 @@ const s3 = new S3({
   region: 'us-east-1',
 });
 
-s3Route.get('/:key', (req, res, next) => {
+s3Route.get('/:name.:ext', (req, res, next) => {
   res.setHeader('Cache-Control', `public, max-age=${3600 * 24 * 30}`); // cache 1 month
+
+  if (['png', 'webp', 'icns', 'ico'].indexOf(req.params.ext) < 0) {
+    next(new Error('404'));
+    return;
+  }
+
+  let contentType;
+  switch (req.params.ext) {
+    case 'png': {
+      contentType = 'image/png';
+      break;
+    }
+    case 'webp': {
+      contentType = 'image/webp';
+      break;
+    }
+    case 'ico': {
+      contentType = 'image/vnd.microsoft.icon'; // real ico, not favicon image/x-icon
+      break;
+    }
+    case 'icns': {
+      contentType = 'image/icns';
+      break;
+    }
+    default: {
+      contentType = 'application/octet-stream';
+    }
+  }
+  res.setHeader('Content-Type', contentType);
 
   const imgStream = s3.getObject({
     Bucket: process.env.S3_BUCKET,
-    Key: req.params.key,
+    Key: `${req.params.name}.${req.params.ext}`,
   }).createReadStream().on('error', next);
 
   imgStream.pipe(res);
