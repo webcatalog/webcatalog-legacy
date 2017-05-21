b10e8f03641c79b28e8ec1905dcdda9695fe4602
Quang Lam <quang.lam2807@gmail.com>
2017-05-20 20:13:40 -0500

    Readd test

---
+++ tests/widevinecdm.js
@@ -0,0 +1,16 @@
+/* global it */
+
+const harness = require('./utils/_harness');
+
+harness('shaka-player-test', () => {
+  it('load widevine', () =>
+    global.app.client
+      .windowByIndex(1) // use webview
+      .waitUntilWindowLoaded()
+      .getText('#drmUsageDrm').should.eventually.equal('widevine'));
+}, [
+  '--testing=true', // disable auto updater
+  '--id=shaka',
+  '--url=https://bitmovin.com/mpeg-dash-hls-drm-test-player/',
+  '--name=Shaka',
+]);
