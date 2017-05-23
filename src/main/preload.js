5ce838caff93ef950bb4e234d5f409faeaa5a24d
Quang Lam <quang.lam2807@gmail.com>
2017-05-23 11:18:32 -0500

    Fix injecting CSS & JS

--- src/main/preload.js
+++ src/main/preload.js
@@ -2,37 +2,33 @@ const { ipcRenderer } = require('electron');
 
 window.onload = () => {
   // inject JS
-  ipcRenderer.once('shell-info', (e, shellInfo) => {
-    window.shellInfo = shellInfo;
+  const shellInfo = ipcRenderer.sendSync('get-shell-info');
 
-    document.title = shellInfo.name;
+  document.title = shellInfo.name;
 
-    const injectedJS = ipcRenderer.sendSync('get-setting', `behaviors.${shellInfo.id}.injectedJS`, null);
-    if (!injectedJS || injectedJS.trim().length < 1) return;
+  const injectedJS = ipcRenderer.sendSync('get-setting', `behaviors.${shellInfo.id}.injectedJS`, null);
+  if (!injectedJS || injectedJS.trim().length < 1) return;
 
-    try {
-      const node = document.createElement('script');
-      node.innerHTML = injectedJS;
-      document.body.appendChild(node);
-    } catch (err) {
-      /* eslint-disable no-console */
-      console.log(err);
-      /* eslint-enable no-console */
-    }
+  try {
+    const node = document.createElement('script');
+    node.innerHTML = injectedJS;
+    document.body.appendChild(node);
+  } catch (err) {
+    /* eslint-disable no-console */
+    console.log(err);
+    /* eslint-enable no-console */
+  }
 
-    const injectedCSS = ipcRenderer.sendSync('get-setting', `behaviors.${shellInfo.id}.injectedCSS`, null);
-    if (!injectedCSS || injectedCSS.trim().length < 1) return;
+  const injectedCSS = ipcRenderer.sendSync('get-setting', `behaviors.${shellInfo.id}.injectedCSS`, null);
+  if (!injectedCSS || injectedCSS.trim().length < 1) return;
 
-    try {
-      const node = document.createElement('style');
-      node.innerHTML = injectedCSS;
-      document.body.appendChild(node);
-    } catch (err) {
-      /* eslint-disable no-console */
-      console.log(err);
-      /* eslint-enable no-console */
-    }
-  });
-
-  ipcRenderer.send('get-shell-info');
+  try {
+    const node = document.createElement('style');
+    node.innerHTML = injectedCSS;
+    document.body.appendChild(node);
+  } catch (err) {
+    /* eslint-disable no-console */
+    console.log(err);
+    /* eslint-enable no-console */
+  }
 };
