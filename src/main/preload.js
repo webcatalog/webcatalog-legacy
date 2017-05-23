9ea57d6597bbc4a378ce6061a00260d891a13f69
Quang Lam <quang.lam2807@gmail.com>
2017-05-23 16:37:44 -0500

    Fix injection bug

--- src/main/preload.js
+++ src/main/preload.js
@@ -7,28 +7,28 @@ window.onload = () => {
   document.title = shellInfo.name;
 
   const injectedJS = ipcRenderer.sendSync('get-setting', `behaviors.${shellInfo.id}.injectedJS`, null);
-  if (!injectedJS || injectedJS.trim().length < 1) return;
-
-  try {
-    const node = document.createElement('script');
-    node.innerHTML = injectedJS;
-    document.body.appendChild(node);
-  } catch (err) {
-    /* eslint-disable no-console */
-    console.log(err);
-    /* eslint-enable no-console */
+  if (injectedJS && injectedJS.trim().length > 0) {
+    try {
+      const node = document.createElement('script');
+      node.innerHTML = injectedJS;
+      document.body.appendChild(node);
+    } catch (err) {
+      /* eslint-disable no-console */
+      console.log(err);
+      /* eslint-enable no-console */
+    }
   }
 
   const injectedCSS = ipcRenderer.sendSync('get-setting', `behaviors.${shellInfo.id}.injectedCSS`, null);
-  if (!injectedCSS || injectedCSS.trim().length < 1) return;
-
-  try {
-    const node = document.createElement('style');
-    node.innerHTML = injectedCSS;
-    document.body.appendChild(node);
-  } catch (err) {
-    /* eslint-disable no-console */
-    console.log(err);
-    /* eslint-enable no-console */
+  if (injectedCSS && injectedCSS.trim().length > 0) {
+    try {
+      const node = document.createElement('style');
+      node.innerHTML = injectedCSS;
+      document.body.appendChild(node);
+    } catch (err) {
+      /* eslint-disable no-console */
+      console.log(err);
+      /* eslint-enable no-console */
+    }
   }
 };
