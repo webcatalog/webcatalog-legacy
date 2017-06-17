a93ebdd3341bd21ce14d177f43b7f32eacc538d6
Quang Lam <quang.lam2807@gmail.com>
2017-06-16 20:16:49 -0500

    Improve UI

--- src/sequelize.js
+++ src/sequelize.js
@@ -1,5 +1,11 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true,
+  },
+});
 
 module.exports = sequelize;
