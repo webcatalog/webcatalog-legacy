235f9b5a2cd635f1f56f432380754c628ca26b38
Mark Andrew <markandrewx@gmail.com>
2017-06-23 20:24:38 -0500

    Adds filter functionality

--- src/renderer/store/components/App/index.js
+++ src/renderer/store/components/App/index.js
@@ -1,37 +1,40 @@
 import React from 'react';
+
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-import { withStyles, createStyleSheet } from 'material-ui/styles';
-import { blue } from 'material-ui/styles/colors';
 import AppBar from 'material-ui/AppBar';
-import Toolbar from 'material-ui/Toolbar';
-import Typography from 'material-ui/Typography';
 import IconButton from 'material-ui/IconButton';
 import SearchIcon from 'material-ui-icons/Search';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import { blue } from 'material-ui/styles/colors';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
 
 import Auth from '../Auth';
+import FilterMenuButton from './FilterMenuButton';
+import getSingularLabel from '../../utils/categories';
+import Home from '../Home';
 import MoreMenuButton from './MoreMenuButton';
 import SortMenuButton from './SortMenuButton';
-import Home from '../Home';
 
 const titleBarHeight = process.env.PLATFORM === 'darwin' ? 21 : 0;
 
 const styleSheet = createStyleSheet('App', {
   root: {
-    width: '100vw',
-    height: '100vh',
-    overflow: 'hidden',
     display: 'flex',
     flexDirection: 'column',
+    height: '100vh',
+    overflow: 'hidden',
+    width: '100vw',
   },
 
   fakeTitleBar: {
     backgroundColor: blue[700],
-    width: '100vw',
     height: titleBarHeight,
-    WebkitUserSelect: 'none',
     WebkitAppRegion: 'drag',
+    WebkitUserSelect: 'none',
+    width: '100vw',
   },
 
   title: {
@@ -42,6 +45,7 @@ const styleSheet = createStyleSheet('App', {
 
 const App = (props) => {
   const {
+    category,
     classes,
     isLoggedIn,
     sortBy,
@@ -49,18 +53,20 @@ const App = (props) => {
   } = props;
 
   const renderTitleElement = () => {
+    const appString = category ? `${getSingularLabel(category)} apps` : 'apps';
+
     let titleText;
     switch (sortBy) {
       case 'installCount': {
-        titleText = sortOrder === 'asc' ? 'Least popular apps' : 'Most popular apps';
+        titleText = sortOrder === 'asc' ? `Least popular ${appString}` : `Most popular ${appString}`;
         break;
       }
       case 'name': {
-        titleText = sortOrder === 'asc' ? 'Apps by name (A-Z)' : 'Apps by name (Z-A)';
+        titleText = sortOrder === 'asc' ? `${appString} by name (A-Z)` : `${appString} by name (Z-A)`;
         break;
       }
       case 'createdAt': {
-        titleText = 'Most recently added apps';
+        titleText = `Most recently added ${appString}`;
         break;
       }
       default: break;
@@ -68,9 +74,9 @@ const App = (props) => {
 
     return (
       <Typography
-        type="title"
-        color="inherit"
         className={classes.title}
+        color="inherit"
+        type="title"
       >
         {titleText}
       </Typography>
@@ -88,27 +94,26 @@ const App = (props) => {
               <SearchIcon />
             </IconButton>
             <SortMenuButton />
+            <FilterMenuButton />
             <MoreMenuButton />
           </Toolbar>
         </AppBar>,
         <Home key="routes" />,
       ] : <Auth />}
-
-
     </div>
   );
 };
 
 App.defaultProps = {
-  sortBy: null,
-  sortOrder: null,
+  category: null,
 };
 
 App.propTypes = {
+  category: PropTypes.string,
   classes: PropTypes.object.isRequired,
   isLoggedIn: PropTypes.bool.isRequired,
-  sortBy: PropTypes.string,
-  sortOrder: PropTypes.string,
+  sortBy: PropTypes.string.isRequired,
+  sortOrder: PropTypes.string.isRequired,
 };
 
 const mapStateToProps = state => ({
