430d6a742c35eda56ff2b183258c35d49b114f35
Mark Andrew <markandrewx@gmail.com>
2017-06-24 22:48:02 -0500

    Replaces MenuItem with ListItem component in MoreMenuButton file

--- src/renderer/store/components/App/MoreMenuButton.js
+++ src/renderer/store/components/App/MoreMenuButton.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
-import Menu, { MenuItem } from 'material-ui/Menu';
+import ArrowBackIcon from 'material-ui-icons/ArrowBack';
+import ArrowForwardIcon from 'material-ui-icons/ArrowForward';
 import Divider from 'material-ui/Divider';
+import HelpIcon from 'material-ui-icons/Help';
 import IconButton from 'material-ui/IconButton';
+import InfoIcon from 'material-ui-icons/Info';
+import Menu from 'material-ui/Menu';
 import MoreVertIcon from 'material-ui-icons/MoreVert';
+import SettingsIcon from 'material-ui-icons/Settings';
+import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 
 class MoreMenuButton extends React.Component {
   constructor() {
@@ -37,12 +43,30 @@ class MoreMenuButton extends React.Component {
           open={this.state.open}
           onRequestClose={this.handleRequestClose}
         >
-          <MenuItem onClick={this.handleRequestClose}>Go Back</MenuItem>
-          <MenuItem onClick={this.handleRequestClose}>Go Forward</MenuItem>
+          <ListItem button onClick={this.handleRequestClose}>
+            <ListItemIcon><ArrowBackIcon /></ListItemIcon>
+            <ListItemText primary="Go back" />
+          </ListItem>
+
+          <ListItem button onClick={this.handleRequestClose}>
+            <ListItemIcon><ArrowForwardIcon /></ListItemIcon>
+            <ListItemText primary="Go forward" />
+          </ListItem>
+
           <Divider light />
-          <MenuItem onClick={this.handleRequestClose}>Settings</MenuItem>
-          <MenuItem onClick={this.handleRequestClose}>Help</MenuItem>
-          <MenuItem onClick={this.handleRequestClose}>About</MenuItem>
+
+          <ListItem button onClick={this.handleRequestClose}>
+            <ListItemIcon><SettingsIcon /></ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+          <ListItem button onClick={this.handleRequestClose}>
+            <ListItemIcon><HelpIcon /></ListItemIcon>
+            <ListItemText primary="Help" />
+          </ListItem>
+          <ListItem button onClick={this.handleRequestClose}>
+            <ListItemIcon><InfoIcon /></ListItemIcon>
+            <ListItemText primary="About" />
+          </ListItem>
         </Menu>
       </div>
     );
