bb6bf38388f2e43610c23acddf6288df30c695b9
Quang Lam <quang.lam2807@gmail.com>
2017-07-26 18:02:31 -0500

    Adds feedback api

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -3,13 +3,15 @@ import express from 'express';
 import appApi from './appApiRouter';
 import authApi from './authApiRouter';
 import draftApi from './draftApiRouter';
+import feedbackApi from './feedbackApiRouter';
 import userApi from './userApiRouter';
 
 const apiRouter = express.Router();
 
 apiRouter.use('/apps', appApi);
-apiRouter.use('/user', userApi);
-apiRouter.use('/drafts', draftApi);
 apiRouter.use('/auth', authApi);
+apiRouter.use('/drafts', draftApi);
+apiRouter.use('/feedback', feedbackApi);
+apiRouter.use('/user', userApi);
 
 module.exports = apiRouter;
