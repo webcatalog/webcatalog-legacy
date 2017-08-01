3dd6b535fc8c73e2859c37ac7a42d6521e692a0b
Quang Lam <quang.lam2807@gmail.com>
2017-08-01 18:28:09 -0500

    0.1.2 - Fixes npm install

---
+++ test/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+const path = require('path');
+
+const createAppAsync = require('../lib');
+
+createAppAsync(
+  'google',
+  'Google',
+  'https://google.com',
+  path.resolve(__dirname, '828296a5-0969-4a56-8e68-e188b03584b0.icns'),
+  path.resolve(__dirname, 'dist'),
+)
+.then(() => console.log('ok'))
+.then(err => console.log(err));
