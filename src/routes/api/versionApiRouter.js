87ed5ccc00bdcd29a5a60b7b26ed7dfd354fc56a
Quang Lam <quang.lam2807@gmail.com>
2017-09-08 17:54:43 +0700

    Updates /api/versions

--- src/routes/api/versionApiRouter.js
+++ src/routes/api/versionApiRouter.js
@@ -1,19 +1,34 @@
 import express from 'express';
+import fetch from 'node-fetch';
 
 const versionApiRouter = express.Router();
 
-versionApiRouter.get('/latest', (req, res) => {
-  res.json({
-    moleculeVersion: '1.0.0',
-    version: process.env.VERSION,
-  });
+versionApiRouter.get('/latest', (req, res, next) => {
+  fetch(`https://raw.githubusercontent.com/webcatalog/webcatalog/v${process.env.VERSION}/package.json`)
+    .then(response => response.text())
+    .then((packageJson) => {
+      const { version, dependencies } = packageJson;
+
+      res.json({
+        moleculeVersion: dependencies['@webcatalog/molecule'],
+        version,
+      });
+    })
+    .catch(next);
 });
 
-versionApiRouter.get('/beta', (req, res) => {
-  res.json({
-    moleculeVersion: '1.0.0',
-    version: process.env.VERSION_BETA,
-  });
+versionApiRouter.get('/beta', (req, res, next) => {
+  fetch(`https://raw.githubusercontent.com/webcatalog/webcatalog/v${process.env.BETA_VERSION}/package.json`)
+    .then(response => response.text())
+    .then((packageJson) => {
+      const { version, dependencies } = packageJson;
+
+      res.json({
+        moleculeVersion: dependencies['@webcatalog/molecule'],
+        version,
+      });
+    })
+    .catch(next);
 });
 
 module.exports = versionApiRouter;
