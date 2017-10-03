80709c161d9055dee6adcaeac41cb76030fdc853
Quang Lam <quang.lam2807@gmail.com>
2017-10-04 00:51:31 +0700

    Adds widevine

--- public/libs/install-app-async/index.js
+++ public/libs/install-app-async/index.js
@@ -3,57 +3,87 @@ const path = require('path');
 const fork = require('child_process').fork;
 const fs = require('fs-extra');
 
+const widevine = require('electron-widevinecdm');
+
 const getAllAppPath = require('../get-all-app-path');
+const sendMessageToWindow = require('../send-message-to-window');
+
+let downloadingWidevine = false;
 
 const installAppAsync = appObj =>
-  new Promise((resolve, reject) => {
-    const { id, name, url, icnsIconUrl, icoIconUrl, pngIconUrl } = appObj;
+  Promise.resolve()
+    .then(() => {
+      const widevinePath = path.join(app.getPath('home'), '.webcatalog', 'widevine');
 
-    const destPath = getAllAppPath();
-    const scriptPath = path.join(__dirname, 'script.js').replace('app.asar', 'app.asar.unpacked');
+      return widevine.checkForUpdateAsync(widevinePath)
+        .then((hasUpdate) => {
+          if (hasUpdate) {
+            if (!downloadingWidevine) {
+              sendMessageToWindow('log', 'Downloading Widevine...');
+              return fs.ensureDir(widevinePath)
+                .then(() => {
+                  downloadingWidevine = true;
+                })
+                .then(() => widevine.downloadAsync(app, widevinePath))
+                .then(() => {
+                  downloadingWidevine = false;
+                });
+            }
+          }
 
-    const child = fork(scriptPath, [
-      '--id',
-      id,
-      '--name',
-      name,
-      '--url',
-      url,
-      '--icnsIconUrl',
-      icnsIconUrl,
-      '--icoIconUrl',
-      icoIconUrl,
-      '--pngIconUrl',
-      pngIconUrl,
-      '--destPath',
-      destPath,
-      '--desktopPath',
-      app.getPath('desktop'),
-      '--homePath',
-      app.getPath('home'),
-    ], {
-      env: {
-        ELECTRON_RUN_AS_NODE: 'true',
-        ELECTRON_NO_ASAR: 'true',
-      },
-    });
+          sendMessageToWindow('log', 'You have the latest version of Widevine');
+          return null;
+        });
+    })
+    .then(() =>
+      new Promise((resolve, reject) => {
+        const { id, name, url, icnsIconUrl, icoIconUrl, pngIconUrl } = appObj;
 
-    child.on('exit', (code) => {
-      if (code === 1) {
-        reject(new Error('failed'));
-        return;
-      }
+        const destPath = getAllAppPath();
+        const scriptPath = path.join(__dirname, 'script.js').replace('app.asar', 'app.asar.unpacked');
 
-      // get current molecule version
-      fs.readJson(path.join(app.getAppPath(), 'package.json'))
-        .then((packageJson) => {
-          const finalizedAppObj = Object.assign({}, appObj, {
-            moleculeVersion: packageJson.dependencies['@webcatalog/molecule'],
-          });
-          resolve(finalizedAppObj);
-        })
-        .catch(reject);
-    });
-  });
+        const child = fork(scriptPath, [
+          '--id',
+          id,
+          '--name',
+          name,
+          '--url',
+          url,
+          '--icnsIconUrl',
+          icnsIconUrl,
+          '--icoIconUrl',
+          icoIconUrl,
+          '--pngIconUrl',
+          pngIconUrl,
+          '--destPath',
+          destPath,
+          '--desktopPath',
+          app.getPath('desktop'),
+          '--homePath',
+          app.getPath('home'),
+        ], {
+          env: {
+            ELECTRON_RUN_AS_NODE: 'true',
+            ELECTRON_NO_ASAR: 'true',
+          },
+        });
+
+        child.on('exit', (code) => {
+          if (code === 1) {
+            reject(new Error('failed'));
+            return;
+          }
+
+          // get current molecule version
+          fs.readJson(path.join(app.getAppPath(), 'package.json'))
+            .then((packageJson) => {
+              const finalizedAppObj = Object.assign({}, appObj, {
+                moleculeVersion: packageJson.dependencies['@webcatalog/molecule'],
+              });
+              resolve(finalizedAppObj);
+            })
+            .catch(reject);
+        });
+      }));
 
 module.exports = installAppAsync;
