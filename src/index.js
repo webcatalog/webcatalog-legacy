5666dbe5aa2696ae66a22ab5a880f4712aa11954
Quang Lam <quang.lam2807@gmail.com>
2017-10-21 15:04:42 +0700

    Improves createAppAsync

--- src/index.js
+++ src/index.js
@@ -14,84 +14,89 @@ const createTmpDirAsync = () =>
     });
   });
 
-const createAppAsync = (id, name, url, icon, out) =>
-  createTmpDirAsync()
-    .then((tmpObj) => {
-      const appDir = path.resolve(__dirname, '..', 'app').replace('app.asar', 'app.asar.unpacked');
-      const tmpCodeDir = path.resolve(tmpObj.dirPath, 'code');
-      const tmpDistDir = path.resolve(tmpObj.dirPath, 'dist');
+const createAppAsync = (id, name, url, icon, out) => {
+  const appDir = path.resolve(__dirname, '..', 'app').replace('app.asar', 'app.asar.unpacked');
 
-      return fs.ensureDir(tmpCodeDir)
-        .then(() => fs.copy(appDir, tmpCodeDir))
-        .then(() => {
-          const packageJsonPath = path.resolve(tmpCodeDir, 'package.json');
-          return fs.readJson(packageJsonPath)
-            .then((packageJsonTemplate) => {
-              const packageJson = Object.assign({}, packageJsonTemplate, {
-                name: id,
-                productName: name,
-                webApp: {
-                  id,
-                  name,
-                  url,
-                },
-              });
-              return fs.writeJson(packageJsonPath, packageJson);
-            })
-            .then(() => fs.ensureDir(tmpDistDir))
-            .then(() =>
-              new Promise((resolve, reject) => {
-                const options = {
-                  name,
-                  icon,
-                  platform: process.platform,
-                  dir: tmpCodeDir,
-                  out: tmpDistDir,
-                  overwrite: true,
-                  prune: false,
-                  asar: {
-                    unpack: 'package.json',
-                  },
-                };
+  let cleanupCallback;
 
-                return packager(options, (err, appPaths) => {
-                  if (err) {
-                    return reject(err);
-                  }
+  return createTmpDirAsync()
+    .then(tmpObj =>
+      new Promise((resolve, reject) => {
+        cleanupCallback = tmpObj.cleanupCallback;
 
-                  return resolve(appPaths);
-                });
-              }),
-            )
-            .then((appPaths) => {
-              if (process.platform === 'darwin') {
-                const binaryFileName = `${name}.app`;
-                const destPath = path.resolve(out, binaryFileName);
+        const options = {
+          name,
+          icon,
+          platform: process.platform,
+          dir: appDir,
+          out: tmpObj.dirPath,
+          overwrite: true,
+          prune: false,
+          asar: {
+            unpack: 'package.json',
+          },
+        };
 
-                return fs.move(
-                  path.resolve(appPaths[0], binaryFileName),
-                  destPath,
-                  { overwrite: true },
-                ).then(() => destPath);
-              }
+        return packager(options, (err, appPaths) => {
+          if (err) {
+            return reject(err);
+          }
 
-              if (process.platform === 'win32' || process.platform === 'linux') {
-                const destPath = path.resolve(out, id);
-
-                return fs.move(
-                  appPaths[0],
-                  destPath,
-                  { overwrite: true },
-                ).then(() => destPath);
-              }
-
-              return Promise.reject(new Error('Unknown platform'));
-            });
-        })
-        .then((destPath) => {
-          tmpObj.cleanupCallback();
-          return destPath;
+          return resolve(appPaths);
         });
+      }),
+    )
+    .then((appPaths) => {
+      if (process.platform === 'darwin') {
+        const binaryFileName = `${name}.app`;
+        const destPath = path.resolve(out, binaryFileName);
+
+        return fs.move(
+          path.resolve(appPaths[0], binaryFileName),
+          destPath,
+          { overwrite: true },
+        ).then(() => destPath);
+      }
+
+      if (process.platform === 'win32' || process.platform === 'linux') {
+        const destPath = path.resolve(out, id);
+
+        return fs.move(
+          appPaths[0],
+          destPath,
+          { overwrite: true },
+        ).then(() => destPath);
+      }
+
+      return Promise.reject(new Error('Unknown platform'));
+    })
+    .then((destPath) => {
+      cleanupCallback();
+
+      const packageJsonPath = process.platform === 'darwin' ?
+        path.resolve(destPath, 'Contents', 'Resources', 'app.asar.unpacked', 'package.json')
+        : path.resolve(destPath, 'app.asar.unpacked', 'package.json');
+
+
+      return fs.readJson(packageJsonPath)
+        .then((packageJsonTemplate) => {
+          const packageJson = Object.assign({}, packageJsonTemplate, {
+            name: id,
+            productName: name,
+            webApp: {
+              id,
+              name,
+              url,
+            },
+          });
+          return fs.writeJson(packageJsonPath, packageJson);
+        })
+        .then(() => destPath);
+    })
+    .catch((err) => {
+      cleanupCallback();
+      return Promise.reject(err);
     });
+};
 
 module.exports = createAppAsync;
