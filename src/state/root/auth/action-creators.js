fa3e942dafeec6f3299be4361c94ac59359ffc69
Quang Lam <quang.lam2807@gmail.com>
2017-10-28 22:41:47 +0700

    Removes Intercom

--- src/state/root/auth/action-creators.js
+++ src/state/root/auth/action-creators.js
@@ -2,13 +2,7 @@ import {
   AUTH_SET_TOKEN,
 } from '../../../constants/actions';
 
-export const authSetToken = (token) => {
-  if (token === null) {
-    window.Intercom('shutdown');
-  }
-
-  return {
-    type: AUTH_SET_TOKEN,
-    token,
-  };
-};
+export const authSetToken = token => ({
+  type: AUTH_SET_TOKEN,
+  token,
+});
--- src/state/root/user/action-creators.js
+++ src/state/root/user/action-creators.js
@@ -8,20 +8,10 @@ export const userGetRequest = () => ({
   type: USER_GET_REQUEST,
 });
 
-export const userGetSuccess = (res) => {
-  window.Intercom('boot', {
-    app_id: 'rzjr1fun',
-    email: res.email,
-    user_id: res.id,
-    user_hash: res.intercomUserHash,
-    signed_up_at: Math.floor(new Date(res.createdAt).getTime() / 1000),
-  });
-
-  return {
-    type: USER_GET_SUCCESS,
-    res,
-  };
-};
+export const userGetSuccess = res => ({
+  type: USER_GET_SUCCESS,
+  res,
+});
 
 export const userGetFailed = () => ({
   type: USER_GET_FAILED,
