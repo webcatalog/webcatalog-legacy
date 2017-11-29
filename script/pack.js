a3e99609a049ac95301d5759e292836cf696df4a
Quang Lam <quang.lam2807@gmail.com>
2017-11-29 11:52:14 +0700

    Adds build scripts

---
+++ script/pack.js
@@ -0,0 +1,40 @@
+// https://github.com/electron/electron-apps/blob/master/script/pack.js
+
+const fs = require('fs-extra');
+const path = require('path');
+const yaml = require('yamljs');
+const sharp = require('sharp');
+
+const apps = [];
+const appPath = path.join(__dirname, '../apps');
+const distPath = path.join(__dirname, '../dist');
+
+fs.readdirSync(appPath)
+  .filter(filename => fs.statSync(path.join(appPath, filename)).isDirectory())
+  .forEach((slug) => {
+    const yamlFile = path.join(appPath, `${slug}/${slug}.yml`);
+    const app = Object.assign(
+      { slug, objectID: slug },
+      yaml.load(yamlFile),
+      {
+        icon: `${slug}-icon.png`,
+        icon128: `${slug}-icon-128.png`,
+      },
+    );
+
+    const iconFile = path.join(appPath, `${slug}/${slug}-icon.png`);
+    fs.copySync(iconFile, path.join(distPath, `${slug}/${slug}-icon.png`));
+
+    sharp(iconFile)
+      .resize(128, 128)
+      .toFile(path.join(distPath, `${slug}/${slug}-icon-128.png`));
+
+    apps.push(app);
+  });
+
+fs.ensureDirSync(distPath);
+
+fs.writeFileSync(
+  path.join(distPath, 'index.json'),
+  JSON.stringify(apps, null, 2),
+);
---
+++ script/publish.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-console */
+
+const apps = require('../dist/index.json');
+const algoliasearch = require('algoliasearch');
+
+const client = algoliasearch(process.env.ALGOLIA_APPLICATION_ID, process.env.ALGOLIA_ADMIN_KEY);
+
+const indexName = 'apps';
+const tmpIndexName = `tmp_apps_${Date.now().toString()}`;
+
+const tmpIndex = client.initIndex(tmpIndexName);
+tmpIndex.addObjects(apps)
+  .then(() => client.moveIndex(tmpIndexName, indexName))
+  .then((err) => {
+    console.log(err);
+    process.exit(1);
+  });
