172a139ab30fec5287169129572fbdca69db7f0f
Quang Lam <quang.lam2807@gmail.com>
2017-12-06 18:11:05 +0700

    Removes createTmpDirAsync & cleanupCallback

--- src/index.js
+++ src/index.js
@@ -7,17 +7,6 @@ const path = require('path');
 const readChunk = require('read-chunk');
 const tmp = require('tmp');
 
-const createTmpDirAsync = () =>
-  new Promise((resolve, reject) => {
-    tmp.dir({ unsafeCleanup: true }, (err, dirPath, cleanupCallback) => {
-      if (err) {
-        return reject(err);
-      }
-
-      return resolve({ dirPath, cleanupCallback });
-    });
-  });
-
 const getIconFileExt = () => {
   switch (process.platform) {
     case 'darwin': return 'icns';
@@ -38,69 +27,61 @@ const resizeAsync = (inputPath, outputPath, newSize) =>
       });
   });
 
-const generateIconAsync = (inputPath) => {
-  const buffer = readChunk.sync(inputPath, 0, 4100);
+const createAppAsync = (id, name, url, pngIcon, out) => {
+  const appDir = path.resolve(__dirname, '..', 'app').replace('app.asar', 'app.asar.unpacked');
+  let tmpDir;
 
-  const type = fileType(buffer);
-  const inputFormat = type.ext;
+  return Promise.resolve()
+    .then(() => {
+      const tmpObj = tmp.dirSync();
+      tmpDir = tmpObj.name;
+    })
+    .then(() => {
+      const buffer = readChunk.sync(pngIcon, 0, 4100);
 
-  const expectedFormat = getIconFileExt();
+      const type = fileType(buffer);
+      const inputFormat = type.ext;
 
-  if (inputFormat !== 'png') {
-    return Promise.reject(new Error('Input format is not supported.'));
-  }
+      const expectedFormat = getIconFileExt();
 
-  if (inputFormat === expectedFormat) return Promise.resolve(inputPath);
+      if (inputFormat !== 'png') {
+        return Promise.reject(new Error('Input format is not supported.'));
+      }
 
-  return createTmpDirAsync()
-    .then((tmpObj) => {
-      const { dirPath } = tmpObj;
+      if (inputFormat === expectedFormat) return Promise.resolve(pngIcon);
 
       const sizes = [16, 24, 32, 48, 64, 128, 256, 512, 1024];
 
-      const p = sizes.map(size => resizeAsync(inputPath, path.join(dirPath, `${size}.png`), size));
+      const p = sizes.map(size => resizeAsync(pngIcon, path.join(tmpDir, `${size}.png`), size));
 
       return Promise.all(p)
-        .then(() => icongen(dirPath, dirPath, { type: 'png', modes: [expectedFormat] }))
+        .then(() => icongen(tmpDir, tmpDir, { type: 'png', modes: [expectedFormat] }))
         .then(results => results[0]);
-    });
-};
-
-const createAppAsync = (id, name, url, pngIcon, out) => {
-  const appDir = path.resolve(__dirname, '..', 'app').replace('app.asar', 'app.asar.unpacked');
-
-  let cleanupCallback;
-
-  return generateIconAsync(pngIcon)
+    })
     .then(icon =>
-      createTmpDirAsync()
-        .then(tmpObj =>
-          new Promise((resolve, reject) => {
-            cleanupCallback = tmpObj.cleanupCallback;
+      new Promise((resolve, reject) => {
+        const options = {
+          name,
+          icon,
+          platform: process.platform,
+          dir: appDir,
+          out: tmpDir,
+          overwrite: true,
+          prune: false,
+          asar: {
+            unpack: 'package.json',
+            unpackDir: path.join('node_modules', 'electron-widevinecdm', 'widevine'),
+          },
+        };
 
-            const options = {
-              name,
-              icon,
-              platform: process.platform,
-              dir: appDir,
-              out: tmpObj.dirPath,
-              overwrite: true,
-              prune: false,
-              asar: {
-                unpack: 'package.json',
-                unpackDir: path.join('node_modules', 'electron-widevinecdm', 'widevine'),
-              },
-            };
+        return packager(options, (err, appPaths) => {
+          if (err) {
+            return reject(err);
+          }
 
-            return packager(options, (err, appPaths) => {
-              if (err) {
-                return reject(err);
-              }
-
-              return resolve(appPaths);
-            });
-          }),
-        ),
+          return resolve(appPaths);
+        });
+      }),
     )
     .then((appPaths) => {
       if (process.platform === 'darwin') {
@@ -127,8 +108,6 @@ const createAppAsync = (id, name, url, pngIcon, out) => {
       return Promise.reject(new Error('Unknown platform'));
     })
     .then((destPath) => {
-      cleanupCallback();
-
       let resourcesPath;
       switch (process.platform) {
         case 'darwin':
@@ -168,10 +147,7 @@ const createAppAsync = (id, name, url, pngIcon, out) => {
         })
         .then(() => destPath);
     })
-    .catch((err) => {
-      cleanupCallback();
-      return Promise.reject(err);
-    });
+    .catch(err => Promise.reject(err));
 };
 
 module.exports = createAppAsync;
