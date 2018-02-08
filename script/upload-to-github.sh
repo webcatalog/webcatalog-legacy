4cf9946a255987d71c00fc15aee8df81b99d9be5
Quang Lam <quang.lam2807@gmail.com>
2018-02-07 19:01:52 -0600

    Add -f

--- script/upload-to-github.sh
+++ script/upload-to-github.sh
@@ -4,4 +4,4 @@ git checkout --orphan icons
 git add .
 git commit -m "Init"
 git remote add origin https://$GH_TOKEN@github.com/quanglam2807/webcatalog-apps.git
-git push origin icons
+git push origin icons -f
