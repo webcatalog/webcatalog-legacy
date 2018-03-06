cbabe3af3e5cfe6f4e12b37fd73f957909b55370
Quang Lam <quang.lam2807@gmail.com>
2018-03-05 19:27:45 -0600

    Fix installing apps hangs forever (#267)

--- appifier-gui/hacky.js
+++ appifier-gui/hacky.js
@@ -8,8 +8,8 @@ const replace = require('replace-in-file');
 // Keep electron-packager
 replace({
   files: path.join(__dirname, 'node_modules', 'electron-builder-lib', 'out', 'util', 'packageDependencies.js'),
-  from: '"electron-builder-tslint-config", "electron-download", "electron-forge", "electron-packager", "electron-compilers", "jest", "jest-cli", "prebuild-install", "nan", "electron-webpack", "electron-webpack-ts", "electron-webpack-vue", "react-scripts"',
-  to: '"electron-builder-tslint-config", "electron-forge", "electron-compilers", "jest", "jest-cli", "prebuild-install", "nan", "electron-webpack", "electron-webpack-ts", "electron-webpack-vue", "react-scripts"',
+  from: '"electron-builder-tslint-config", "electron-download", "electron-forge", "electron-packager", "electron-compilers", "prebuild-install", "nan", "electron-webpack", "electron-webpack-ts", "electron-webpack-vue", "@types"',
+  to: '"electron-builder-tslint-config", "electron-forge", "electron-compilers", "prebuild-install", "nan", "electron-webpack", "electron-webpack-ts", "electron-webpack-vue", "@types"',
 })
   .then((changedFiles) => {
     console.log('Modified files:', changedFiles.join(', '));
