ac16b581c7d0493884773a5e34a86362c01c1247
Quang Lam <quang.lam2807@gmail.com>
2018-03-08 14:44:42 -0600

    Fix app uninstallation doesn't work correctly on Linux (#17)

--- public/libs/uninstall-app-async/index.js
+++ public/libs/uninstall-app-async/index.js
@@ -1,11 +1,32 @@
+const { app } = require('electron');
 const path = require('path');
 const fs = require('fs-extra');
 
 const getInstallationPath = require('../get-installation-path');
 
 const uninstallAppAsync = (appId, appName) => {
-  const appPath = path.join(getInstallationPath(), `${appName}.app`);
-  return fs.remove(appPath);
+  const p = [];
+
+  if (process.platform === 'darwin') {
+    const appPath = path.join(getInstallationPath(), `${appName}.app`);
+    p.push(fs.remove(appPath));
+
+    const dataPath = path.join(app.getPath('home'), '.webcatalog-lite', appId);
+    p.push(fs.remove(dataPath));
+  } else if (process.platform === 'linux') {
+    const binPath = path.join(getInstallationPath(), `${appId}`);
+    p.push(fs.remove(binPath));
+
+    const desktopPath = path.join(app.getPath('home'), '.local', 'share', 'applications', `webcatalog-${appId}.desktop`);
+    p.push(fs.remove(desktopPath));
+
+    const dataPath = path.join(app.getPath('home'), '.config', `webcatalog-${appId}`);
+    p.push(fs.remove(dataPath));
+  } else {
+    return Promise.reject(new Error('Platform undefined.'));
+  }
+
+  return Promise.all(p);
 };
 
 module.exports = uninstallAppAsync;
