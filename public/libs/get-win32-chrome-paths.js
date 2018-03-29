23ffce2dc0def847badcc1e2961f69ba6320793e
Quang Lam <quang.lam2807@gmail.com>
2018-03-29 18:04:30 -0500

    Add Windows support (2)

---
+++ public/libs/get-win32-chrome-paths.js
@@ -0,0 +1,35 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+const canAccess = (file) => {
+  if (!file) {
+    return false;
+  }
+
+  try {
+    fs.accessSync(file);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+const getWin32ChromePaths = () => {
+  const installations = [];
+  const suffixes = [
+    `${path.sep}Google${path.sep}Chrome SxS${path.sep}Application${path.sep}chrome.exe`,
+    `${path.sep}Google${path.sep}Chrome${path.sep}Application${path.sep}chrome.exe`,
+  ];
+  const prefixes = [process.env.LOCALAPPDATA, process.env.PROGRAMFILES, process.env['PROGRAMFILES(X86)']].filter(Boolean);
+
+  prefixes.forEach(prefix => suffixes.forEach((suffix) => {
+    const chromePath = path.join(prefix, suffix);
+    if (canAccess(chromePath)) {
+      installations.push(chromePath);
+    }
+  }));
+
+  return installations;
+};
+
+module.exports = getWin32ChromePaths;
--- public/listeners/generic.js
+++ public/listeners/generic.js
@@ -3,12 +3,16 @@ const path = require('path');
 const fs = require('fs');
 const commandExistsSync = require('command-exists').sync;
 
+const getWin32ChromePaths = require('../libs/get-win32-chrome-paths');
+
 const loadGenericListeners = () => {
   ipcMain.on('request-open-in-browser', (e, browserUrl) => {
     shell.openExternal(browserUrl);
   });
 
   ipcMain.on('is-chrome-installed', (e, browser) => {
+    console.log(getWin32ChromePaths());
+
     switch (browser) {
       case 'chromium': {
         if (process.platform === 'darwin') {
