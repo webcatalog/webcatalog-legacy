0e3234274acec1122e4e7e70c50fd5e3f3ebd071
Quang Lam <quang.lam2807@gmail.com>
2018-05-27 15:48:19 -0500

    Upgrade to electron@2.0 (#67)

--- tests/start-up.js
+++ tests/start-up.js
@@ -5,8 +5,6 @@ const harness = require('./utils/_harness');
 harness('store-test', () => {
   it('Load WebCatalog', () =>
     global.app.client
-      .windowByIndex(0)
-      .waitUntilWindowLoaded()
       .waitForVisible('.jss1'));
 }, [
   '--testing=true',
