85eab3bdad296b27a153623527e097ac3198846b
Quang Lam <quang.lam2807@gmail.com>
2018-06-16 19:05:57 -0500

    Add engine comparision table

--- src/components/root/browser-prompt.js
+++ src/components/root/browser-prompt.js
@@ -10,9 +10,14 @@ import connectComponent from '../../helpers/connect-component';
 import {
   STRING_SELECT_BROWSER,
   STRING_SELECT_BROWSER_DESC,
+  STRING_SELECT_BROWSER_DESC_2,
+  STRING_LEARN_MORE,
+  STRING_RECOMMENDED,
 } from '../../constants/strings';
 
 import { updatePreference } from '../../state/root/preferences/actions';
+
+import { requestOpenInBrowser } from '../../senders/generic';
 
 import chromeIcon from '../../assets/chrome.png';
 import chromiumIcon from '../../assets/chromium.png';
@@ -35,6 +40,7 @@ const styles = theme => ({
     display: 'flex',
     justifyContent: 'center',
     flexWrap: 'wrap',
+    marginTop: theme.spacing.unit * 3,
   },
   card: {
     cursor: 'pointer',
@@ -50,6 +56,13 @@ const styles = theme => ({
     height: 64,
     width: 64,
   },
+  link: {
+    color: theme.palette.primary.dark,
+    cursor: 'pointer',
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
 });
 
 const BrowserPrompt = (props) => {
@@ -61,7 +74,9 @@ const BrowserPrompt = (props) => {
   return (
     <div className={classes.root}>
       <div className={classes.box}>
-        <p>{STRING_SELECT_BROWSER}</p>
+        <Typography variant="title">
+          {STRING_SELECT_BROWSER}
+        </Typography>
 
         <div className={classes.cardContainer}>
           <Card className={classes.card} onClick={() => onUpdatePreference('browser', 'juli')}>
@@ -69,6 +84,9 @@ const BrowserPrompt = (props) => {
               <img src={juliIcon} className={classes.browserIcon} alt="Juli" />
               <Typography variant="subheading">
                 Juli
+              </Typography>
+              <Typography variant="body1">
+                ({STRING_RECOMMENDED})
               </Typography>
             </CardContent>
           </Card>
@@ -104,6 +122,19 @@ const BrowserPrompt = (props) => {
         </div>
 
         <div className={classes.clear} />
+   
+        <p>
+          <span>{STRING_SELECT_BROWSER_DESC_2} </span>
+          <span
+            onClick={() => requestOpenInBrowser('https://getwebcatalog.com/engines')}
+            onKeyDown={() => requestOpenInBrowser('https://getwebcatalog.com/engines')}
+            role="link"
+            tabIndex="0"
+            className={classes.link}
+          >
+            {STRING_LEARN_MORE}
+          </span>.
+        </p>
 
         <p>{STRING_SELECT_BROWSER_DESC}</p>
       </div>
