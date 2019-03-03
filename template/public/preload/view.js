4833499c0b40657e9c60df32c2fe5d5cd5ffb868
Quang Lam <quang.lam2807@gmail.com>
2019-03-02 21:07:07 -0600

    Remove console.log

--- template/public/preload/view.js
+++ template/public/preload/view.js
@@ -66,7 +66,6 @@ window.onload = () => {
   const linkPreview = document.createElement('div');
   linkPreview.style.cssText = 'max-width: 80vw;height: 22px;position: fixed;bottom: -1px;right: -1px;z-index: 1000000;background-color: rgb(245, 245, 245);border-radius: 2px;border: #9E9E9E  1px solid;font-size: 12.5px;color: rgb(0, 0, 0);padding: 0px 8px;line-height: 22px;font-family: -apple-system, system-ui, BlinkMacSystemFont, sans-serif;white-space: nowrap;text-overflow: ellipsis;overflow: hidden; pointer-events:none;';
   ipcRenderer.on('update-target-url', (e, url) => {
-    console.log(url);
     if (url) {
       linkPreview.innerText = url;
       document.body.appendChild(linkPreview);
