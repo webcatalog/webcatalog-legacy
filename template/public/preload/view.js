5330692710f7b96b6ca0bfa44151c263a5a5226c
Quang Lam <quang.lam2807@gmail.com>
2019-03-02 22:35:10 -0600

    Add link preview when hovering (#193)

--- template/public/preload/view.js
+++ template/public/preload/view.js
@@ -61,6 +61,18 @@ window.onload = () => {
         menu.popup(remote.getCurrentWindow());
       });
   });
+
+  // Link preview
+  const linkPreview = document.createElement('div');
+  linkPreview.style.cssText = 'max-width: 80vw;height: 22px;position: fixed;bottom: -1px;right: -1px;z-index: 1000000;background-color: rgb(245, 245, 245);border-radius: 2px;border: #9E9E9E  1px solid;font-size: 12.5px;color: rgb(0, 0, 0);padding: 0px 8px;line-height: 22px;font-family: -apple-system, system-ui, BlinkMacSystemFont, sans-serif;white-space: nowrap;text-overflow: ellipsis;overflow: hidden; pointer-events:none;';
+  ipcRenderer.on('update-target-url', (e, url) => {
+    if (url) {
+      linkPreview.innerText = url;
+      document.body.appendChild(linkPreview);
+    } else {
+      document.body.removeChild(linkPreview);
+    }
+  });
 };
 
 // Fix Can't show file list of Google Drive
