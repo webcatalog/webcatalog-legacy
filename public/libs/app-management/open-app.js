1b6b47387bd4e014f8b6adc06ba214d1dabf66ad
Quang Lam <quang.lam2807@gmail.com>
2019-08-21 20:30:08 +0800

    disable open app on Snap

--- public/libs/app-management/open-app.js
+++ public/libs/app-management/open-app.js
@@ -1,8 +1,9 @@
-const { app, shell } = require('electron');
+const { app, dialog, shell } = require('electron');
 const path = require('path');
 const { exec } = require('child_process');
 
 const { getPreference } = require('../preferences');
+const mainWindow = require('../../windows/main');
 
 const openApp = (id, name) => {
   let appPath;
@@ -10,7 +11,14 @@ const openApp = (id, name) => {
     appPath = path.join(getPreference('installationPath').replace('~', app.getPath('home')), `${name}.app`);
     shell.openItem(appPath);
   } else if (process.platform === 'linux') {
-    exec(`gtk-launch webcatalog-${id}`);
+    if (process.env.SNAP == null) {
+      exec(`gtk-launch webcatalog-${id}`);
+    } else {
+      dialog.showMessageBox(mainWindow.get(), {
+        type: 'info',
+        message: 'Because of Snap\'s limitation, we cannot launch the app from here. Please use the app launcher instead.',
+      });
+    }
   } else if (process.platform === 'win32') {
     appPath = path.join(getPreference('installationPath'), name, `${name}.exe`);
     shell.openItem(appPath);
