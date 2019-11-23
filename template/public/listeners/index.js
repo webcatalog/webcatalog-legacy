d6ac690d42f84464a146977f8478a5b6f41c359d
Quang Lam <quang.lam2807@gmail.com>
2019-11-22 20:24:20 -0600

    Fix relaunching doesn't run immediately

--- template/public/listeners/index.js
+++ template/public/listeners/index.js
@@ -152,7 +152,7 @@ const loadListeners = () => {
     }).then(({ response }) => {
       if (response === 0) {
         app.relaunch();
-        app.quit();
+        app.exit(0);
       }
     })
     .catch(console.log); // eslint-disable-line
