0396d00954b97625f45d2448c4cf9a55b2a2a16d
Quang Lam <quang.lam2807@gmail.com>
2019-11-29 16:46:17 -0600

    ok

--- template/src/index.js
+++ template/src/index.js
@@ -8,87 +8,74 @@ import 'typeface-roboto/index.css';
 
 import store from './state';
 
-import About from './components/about';
 import AppWrapper from './components/app-wrapper';
-import Auth from './components/auth';
-import CodeInjection from './components/code-injection';
-import EditWorkspace from './components/edit-workspace';
-import Main from './components/main';
-import OpenUrlWith from './components/open-url-with';
-import Preferences from './components/preferences';
 
 import getWorkspacesAsList from './helpers/get-workspaces-as-list';
 
 const { remote, webFrame } = window.require('electron');
 
-webFrame.setVisualZoomLevelLimits(1, 1);
-webFrame.setLayoutZoomLevelLimits(0, 0);
+const About = React.lazy(() => import('./components/about'));
+const Auth = React.lazy(() => import('./components/auth'));
+const CodeInjection = React.lazy(() => import('./components/code-injection'));
+const EditWorkspace = React.lazy(() => import('./components/edit-workspace'));
+const Main = React.lazy(() => import('./components/main'));
+const OpenUrlWith = React.lazy(() => import('./components/open-url-with'));
+const Preferences = React.lazy(() => import('./components/preferences'));
+
+const App = () => {
+  switch (window.mode) {
+    case 'about': return <About />;
+    case 'auth': return <Auth />;
+    case 'code-injection': return <CodeInjection />;
+    case 'edit-workspace': return <EditWorkspace />;
+    case 'open-url-with': return <OpenUrlWith />;
+    case 'preferences': return <Preferences />;
+    default: return <Main />;
+  }
+};
 
 const runApp = () => {
-  let App;
-  switch (window.mode) {
-    case 'preferences': {
-      App = Preferences;
-      break;
-    }
-    case 'edit-workspace': {
-      App = EditWorkspace;
-      break;
-    }
-    case 'open-url-with': {
-      App = OpenUrlWith;
-      break;
-    }
-    case 'code-injection': {
-      App = CodeInjection;
-      break;
-    }
-    case 'auth': {
-      App = Auth;
-      break;
-    }
-    case 'about': {
-      App = About;
-      break;
-    }
-    default: {
-      App = Main;
-    }
-  }
+  Promise.resolve()
+    .then(() => {
+      webFrame.setVisualZoomLevelLimits(1, 1);
+      webFrame.setLayoutZoomLevelLimits(0, 0);
 
-  if (window.mode === 'about') {
-    document.title = 'About';
-  } else if (window.mode === 'preferences') {
-    document.title = 'Preferences';
-  } else if (window.mode === 'edit-workspace') {
-    const { workspaces } = store.getState();
-    const workspaceList = getWorkspacesAsList(workspaces);
-    const editWorkspaceId = window.require('electron').remote.getGlobal('editWorkspaceId');
-    const workspace = workspaces[editWorkspaceId];
-    workspaceList.some((item, index) => {
-      if (item.id === editWorkspaceId) {
-        workspace.order = index;
-        return true;
+      if (window.mode === 'about') {
+        document.title = 'About';
+      } else if (window.mode === 'preferences') {
+        document.title = 'Preferences';
+      } else if (window.mode === 'edit-workspace') {
+        const { workspaces } = store.getState();
+        const workspaceList = getWorkspacesAsList(workspaces);
+        const editWorkspaceId = window.require('electron').remote.getGlobal('editWorkspaceId');
+        const workspace = workspaces[editWorkspaceId];
+        workspaceList.some((item, index) => {
+          if (item.id === editWorkspaceId) {
+            workspace.order = index;
+            return true;
+          }
+          return false;
+        });
+        document.title = workspace.name ? `Edit Workspace ${workspace.order + 1} "${workspace.name}"` : `Edit Workspace ${workspace.order + 1}`;
+      } else if (window.mode === 'open-url-with') {
+        document.title = 'Open Link With';
+      } else if (window.mode === 'code-injection') {
+        const codeInjectionType = window.require('electron').remote.getGlobal('codeInjectionType');
+        document.title = `Edit ${codeInjectionType.toUpperCase()} Code Injection`;
+      } else if (window.mode === 'code-injection') {
+        document.title = 'Sign in';
+      } else {
+        document.title = remote.getGlobal('appJson').name;
       }
-      return false;
     });
-    document.title = workspace.name ? `Edit Workspace ${workspace.order + 1} "${workspace.name}"` : `Edit Workspace ${workspace.order + 1}`;
-  } else if (window.mode === 'open-url-with') {
-    document.title = 'Open Link With';
-  } else if (window.mode === 'code-injection') {
-    const codeInjectionType = window.require('electron').remote.getGlobal('codeInjectionType');
-    document.title = `Edit ${codeInjectionType.toUpperCase()} Code Injection`;
-  } else if (window.mode === 'code-injection') {
-    document.title = 'Sign in';
-  } else {
-    document.title = remote.getGlobal('appJson').name;
-  }
 
   ReactDOM.render(
     <Provider store={store}>
       <AppWrapper>
         <CssBaseline />
-        <App />
+        <React.Suspense fallback={<div />}>
+          <App />
+        </React.Suspense>
       </AppWrapper>
     </Provider>,
     document.getElementById('app'),
