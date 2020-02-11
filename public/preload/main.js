dd9fa65531960e5d42a37dbb3326e9b7ddcc20f9
Quang Lam <quang.lam2807@gmail.com>
2020-02-11 03:49:45 -0600

    Add right-click context menu for selecting/editing (#531)

--- public/preload/main.js
+++ public/preload/main.js
@@ -1,5 +1,3 @@
-window.mode = 'edit-workspace';
-
 const contextMenu = require('electron-context-menu');
 
 contextMenu();
--- public/preload/menubar.js
+++ public/preload/menubar.js
@@ -1,1 +1,5 @@
 window.mode = 'menubar';
+
+const contextMenu = require('electron-context-menu');
+
+contextMenu();
