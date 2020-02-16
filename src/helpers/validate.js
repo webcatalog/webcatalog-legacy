4f589c1dc21afe3267544ed0040a46d7fb70a8b1
Quang Lam <quang.lam2807@gmail.com>
2020-02-16 12:26:09 -0600

    more fix

--- src/helpers/validate.js
+++ src/helpers/validate.js
@@ -10,20 +10,20 @@ const kits = {
     return null;
   },
   url: (val, _, fieldName) => {
-    if (!isUrl(val)) {
+    if (val && !isUrl(val)) {
       return '{fieldName} is not valid.'.replace('{fieldName}', fieldName);
     }
     return null;
   },
   // accept link without protocol prefix
   lessStrictUrl: (val, _, fieldName) => {
-    if (!isUrl(val) && !isUrl(`http://${val}`)) {
+    if (val && !isUrl(val) && !isUrl(`http://${val}`)) {
       return '{fieldName} is not valid.'.replace('{fieldName}', fieldName);
     }
     return null;
   },
   licenseKey: (val, _, fieldName) => {
-    if (!isValidLicenseKey(val)) {
+    if (val && !isValidLicenseKey(val)) {
       return '{fieldName} is not valid.'.replace('{fieldName}', fieldName);
     }
     return null;
