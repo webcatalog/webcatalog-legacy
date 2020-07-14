91f67dab818c67dae614ff3d0ba4eef6d8975793
Quang Lam <quang.lam2807@gmail.com>
2020-07-15 00:05:25 +0700

    Upgrade to electron-packager@15.x (#907)

--- public/libs/get-powershell-major-version-async.js
+++
@@ -1,8 +0,0 @@
-// https://stackoverflow.com/questions/1825585/determine-installed-powershell-version
-const execAsync = require('./exec-async');
-
-const getPowershellMajorVersionAsync = () => execAsync('powershell "$PSVersionTable.PSVersion.Major"')
-  .then((majorVersionString) => parseInt(majorVersionString, 10))
-  .catch(() => 0);
-
-module.exports = getPowershellMajorVersionAsync;
