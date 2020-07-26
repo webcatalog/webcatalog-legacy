d816fb3adcdb1835d9dc95064e38c7eddc7ff245
Quang Lam <quang.lam2807@gmail.com>
2020-07-26 13:34:44 +0700

    Fix "ls: /Applications/WebCatalog Apps: No such file or directory" bug (#929)

--- public/libs/app-management/install-app-async/forked-script-electron.js
+++ public/libs/app-management/install-app-async/forked-script-electron.js
@@ -266,6 +266,9 @@ Promise.resolve()
     // in v20.5.2 and below, '/Applications/WebCatalog Apps' owner is set to `root`
     // need to correct to user to install apps without sudo
     if (installationPath === '/Applications/WebCatalog Apps') {
+      if (!fsExtra.existsSync(installationPath)) {
+        fsExtra.mkdirSync(installationPath);
+      }
       // https://unix.stackexchange.com/a/7732
       const installationPathOwner = await execAsync("ls -ld '/Applications/WebCatalog Apps' | awk '{print $3}'");
       if (installationPathOwner.trim() === 'root') {
