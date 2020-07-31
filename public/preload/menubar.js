947d4fe3e2fb83796997ebef567628db8ac1780d
Quang Lam <quang.lam2807@gmail.com>
2020-07-31 09:32:36 +0700

    Fix home page is blank white page in menubar mode

--- public/preload/menubar.js
+++ public/preload/menubar.js
@@ -1,8 +1,3 @@
 window.mode = 'menubar';
 
-const { remote } = require('electron');
-const contextMenu = require('electron-context-menu');
-
-contextMenu({
-  window: remote.getCurrentWindow(),
-});
+require('./main');
