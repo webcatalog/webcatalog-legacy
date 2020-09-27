c0620945abcc2f1f11fc896812e5e90f3e59d59b
Quang Lam <quang.lam2807@gmail.com>
2020-09-27 16:19:42 +0700

    Update app-card.js

--- src/components/shared/app-card.js
+++ src/components/shared/app-card.js
@@ -80,6 +80,7 @@ const styles = (theme) => ({
   paperIconCatalogWindows: {
     boxShadow: '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
     borderRadius: 12,
+    marginBottom: theme.spacing(1),
   },
   actionContainer: {
     marginTop: theme.spacing(1),
