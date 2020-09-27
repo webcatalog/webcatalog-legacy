dad0a0e16483b41eead9e467c1f4f2b25bb66037
Quang Lam <quang.lam2807@gmail.com>
2020-09-27 16:22:32 +0700

    fix

--- src/components/shared/app-card.js
+++ src/components/shared/app-card.js
@@ -60,7 +60,7 @@ const styles = (theme) => ({
     overflow: 'hidden',
     whiteSpace: 'nowrap',
     textOverflow: 'ellipsis',
-    lineHeight: 1,
+    lineHeight: 'normal',
     marginTop: theme.spacing(1),
     fontWeight: 500,
   },
