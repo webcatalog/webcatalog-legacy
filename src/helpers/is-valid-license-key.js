63495b13cecb1b02dfed33cf423fb5a52a360a36
Quang Lam <quang.lam2807@gmail.com>
2020-10-02 03:03:13 +0700

    Allow users to activate WebCatalog with Singlebox license (#1057)

--- src/helpers/is-valid-license-key.js
+++ src/helpers/is-valid-license-key.js
@@ -7,10 +7,13 @@ const isValidLicenseKey = (licenseKey) => {
     const parts = inputLicenseKey.split('-');
     const quantity = parts[0];
     const time = parts[1];
-    const md5Str = parts.slice(2).join('');
+    const md5Str = parts.slice(2).join('').toUpperCase();
 
     return md5(process.env.REACT_APP_LICENSE_SECRET + quantity + time).toUpperCase()
-      === md5Str.toUpperCase();
+      === md5Str
+      // allow user to activate WebCatalog with Singlebox license
+      || md5(process.env.REACT_APP_LICENSE_SECRET_SINGLEBOX_LEGACY + quantity + time).toUpperCase()
+        === md5Str;
   } catch (err) {
     return false;
   }
