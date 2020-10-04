93f13240888696107961c67abef7680f598dc1ea
Quang Lam <quang.lam2807@gmail.com>
2020-10-04 23:39:24 +0700

    x

--- src/components/pages/home/index.js
+++ src/components/pages/home/index.js
@@ -3,7 +3,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import AppSearchAPIConnector from '@elastic/search-ui-app-search-connector';
-import { SearchProvider, WithSearch, Paging } from '@elastic/react-search-ui';
+import {
+  SearchProvider, WithSearch, Paging, Facet,
+} from '@elastic/react-search-ui';
+import { MultiCheckboxFacet } from '@elastic/react-search-ui-views';
 import '@elastic/react-search-ui-views/lib/styles/styles.css';
 
 import Grid from '@material-ui/core/Grid';
@@ -48,6 +51,10 @@ const styles = (theme) => ({
   noMatchingResultOpts: {
     marginTop: theme.spacing(4),
   },
+  facet: {
+    paddingRight: theme.spacing(2),
+    color: theme.palette.text.primary,
+  },
 });
 
 const Home = ({
@@ -63,6 +70,7 @@ const Home = ({
       },
       alwaysSearchOnInitialLoad: true,
       searchQuery: {
+        disjunctiveFacets: ['type', 'category'],
         result_fields: {
           id: { raw: {} },
           name: { raw: {} },
@@ -72,6 +80,10 @@ const Home = ({
           icon_unplated: window.process.platform === 'win32' ? { raw: {} } : undefined,
           icon_unplated_128: window.process.platform === 'win32' ? { raw: {} } : undefined,
         },
+        facets: {
+          type: { type: 'value', size: 2 },
+          category: { type: 'value', size: 30 },
+        },
       },
     }}
   >
@@ -80,96 +92,116 @@ const Home = ({
       <SecondaryToolbar />
       <Divider />
       <div className={classes.scrollContainer}>
-        <Grid container spacing={1} justify="space-evenly">
-          <WithSearch
-            mapContextToProps={({
-              error,
-              isLoading,
-              results,
-              searchTerm,
-              setSearchTerm,
-              wasSearched,
-            }) => ({
-              error,
-              isLoading,
-              results,
-              searchTerm,
-              setSearchTerm,
-              wasSearched,
-            })}
-          >
-            {({
-              error,
-              isLoading,
-              results,
-              searchTerm,
-              setSearchTerm,
-              wasSearched,
-            }) => {
-              if (error) {
-                return (
-                  <div className={classes.noConnectionContainer}>
-                    <NoConnection
-                      onTryAgainButtonClick={() => {
-                        setSearchTerm(searchTerm, { refresh: true, debounce: 0 });
-                      }}
-                    />
-                  </div>
-                );
-              }
+        <Grid container>
+          <Grid item>
+            <Facet
+              className={classes.facet}
+              field="type"
+              label="Types"
+              view={MultiCheckboxFacet}
+              show={2}
+              filterType="any"
+            />
+            <Facet
+              className={classes.facet}
+              field="category"
+              label="Categories"
+              view={MultiCheckboxFacet}
+              show={30}
+              filterType="any"
+            />
+          </Grid>
+          <Grid item xs container spacing={1} justify="space-evenly">
+            <WithSearch
+              mapContextToProps={({
+                error,
+                isLoading,
+                results,
+                searchTerm,
+                setSearchTerm,
+                wasSearched,
+              }) => ({
+                error,
+                isLoading,
+                results,
+                searchTerm,
+                setSearchTerm,
+                wasSearched,
+              })}
+            >
+              {({
+                error,
+                isLoading,
+                results,
+                searchTerm,
+                setSearchTerm,
+                wasSearched,
+              }) => {
+                if (error) {
+                  return (
+                    <div className={classes.noConnectionContainer}>
+                      <NoConnection
+                        onTryAgainButtonClick={() => {
+                          setSearchTerm(searchTerm, { refresh: true, debounce: 0 });
+                        }}
+                      />
+                    </div>
+                  );
+                }
 
-              if (isLoading && results.length < 1) {
-                return (
-                  <Grid item xs={12}>
-                    <Typography
-                      variant="body2"
-                      align="center"
-                      color="textSecondary"
-                      className={classes.loading}
-                    >
-                      Loading...
-                    </Typography>
-                  </Grid>
-                );
-              }
-
-              if (wasSearched && results.length < 1) {
-                return (
-                  <EmptyState icon={SearchIcon} title="No Matching Results">
-                    <Typography
-                      variant="subtitle1"
-                      align="center"
-                    >
-                      Your query did not match any apps in our database.
-                    </Typography>
-                    <Grid container justify="center" spacing={1} className={classes.noMatchingResultOpts}>
-                      <SubmitAppCard />
+                if (isLoading && results.length < 1) {
+                  return (
+                    <Grid item xs={12}>
+                      <Typography
+                        variant="body2"
+                        align="center"
+                        color="textSecondary"
+                        className={classes.loading}
+                      >
+                        Loading...
+                      </Typography>
                     </Grid>
-                  </EmptyState>
-                );
-              }
+                  );
+                }
 
-              return (
-                <>
-                  {results.map((app) => (
-                    <AppCard
-                      key={app.id.raw}
-                      id={app.id.raw}
-                      name={app.name.raw}
-                      url={app.url.raw}
-                      icon={window.process.platform === 'win32' // use unplated icon for Windows
-                        ? app.icon_unplated.raw : app.icon.raw}
-                      icon128={window.process.platform === 'win32' // use unplated icon for Windows
-                        ? app.icon_unplated_128.raw : app.icon_128.raw}
-                    />
-                  ))}
-                  <Grid item xs={12} container justify="center">
-                    <Paging />
-                  </Grid>
-                </>
-              );
-            }}
-          </WithSearch>
+                if (wasSearched && results.length < 1) {
+                  return (
+                    <EmptyState icon={SearchIcon} title="No Matching Results">
+                      <Typography
+                        variant="subtitle1"
+                        align="center"
+                      >
+                        Your query did not match any apps in our database.
+                      </Typography>
+                      <Grid container justify="center" spacing={1} className={classes.noMatchingResultOpts}>
+                        <SubmitAppCard />
+                      </Grid>
+                    </EmptyState>
+                  );
+                }
+
+                return (
+                  <>
+                    {results.map((app) => (
+                      <AppCard
+                        key={app.id.raw}
+                        id={app.id.raw}
+                        name={app.name.raw}
+                        url={app.url.raw}
+                        icon={window.process.platform === 'win32' // use unplated icon for Windows
+                          ? app.icon_unplated.raw : app.icon.raw}
+                        icon128={window.process.platform === 'win32' // use unplated icon for Windows
+                          ? app.icon_unplated_128.raw : app.icon_128.raw}
+                      />
+                    ))}
+                    <Grid item xs={12} container justify="center">
+                      <Paging />
+                    </Grid>
+                  </>
+                );
+              }}
+            </WithSearch>
+          </Grid>
         </Grid>
       </div>
     </div>
