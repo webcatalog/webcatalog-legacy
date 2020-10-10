e07fbfa84e913445ef7aec93e5a02186ed636f19
Quang Lam <quang.lam2807@gmail.com>
2020-10-10 20:48:32 +0700

    Fix app trial session browsing data is not cleared (#1083)

--- public/windows/app-trial.js
+++ public/windows/app-trial.js
@@ -14,9 +14,11 @@ const create = (id, url, name) => {
     show: true,
     title: name,
     webPreferences: {
+      partition: `try-${id}`,
       enableRemoteModule: false,
       nodeIntegration: false,
       webSecurity: true,
+      scrollBounce: true,
     },
   });
   win.setMenuBarVisibility(false);
