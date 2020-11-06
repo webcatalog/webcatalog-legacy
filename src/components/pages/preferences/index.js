a0ae4473129606472dd48a3b9ee0acf3488d5934
Quang Lam <quang.lam2807@gmail.com>
2020-11-06 16:21:10 +0700

    Disable menu bar mode on Windows & Linux (#1158)

--- src/components/pages/preferences/index.js
+++ src/components/pages/preferences/index.js
@@ -319,28 +319,27 @@ const Preferences = ({
                   <MenuItem dense value="dark">Dark</MenuItem>
                 </Select>
               </ListItem>
-              <Divider />
-              <ListItem>
-                <ListItemText
-                  primary={window.process.platform === 'win32'
-                    ? 'Attach to taskbar' : 'Attach to menu bar'}
-                />
-                <ListItemSecondaryAction>
-                  <Switch
-                    edge="end"
-                    color="primary"
-                    checked={attachToMenubar}
-                    onChange={(e) => {
-                      if (e.target.checked && !registered) {
-                        onOpenDialogLicenseRegistration();
-                        return;
-                      }
-                      requestSetPreference('attachToMenubar', e.target.checked);
-                      requestShowRequireRestartDialog();
-                    }}
-                  />
-                </ListItemSecondaryAction>
-              </ListItem>
+              {window.process.platform === 'darwin' && (
+                <>
+                  <Divider />
+                  <ListItem>
+                    <ListItemText
+                      primary="Attach to menu bar"
+                    />
+                    <ListItemSecondaryAction>
+                      <Switch
+                        edge="end"
+                        color="primary"
+                        checked={attachToMenubar}
+                        onChange={(e) => {
+                          requestSetPreference('attachToMenubar', e.target.checked);
+                          requestShowRequireRestartDialog();
+                        }}
+                      />
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                </>
+              )}
               <Divider />
               <ListItem>
                 <ListItemText
