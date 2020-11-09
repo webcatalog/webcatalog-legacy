9fe92148c7713f2dbdd8f557a33b848ad418b64f
Quang Lam <quang.lam2807@gmail.com>
2020-11-09 23:14:14 +0700

    Replace typeface-roboto with fontsource-roboto (#1161)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import 'typeface-roboto/index.css';
+import 'fontsource-roboto/400.css';
+import 'fontsource-roboto/500.css';
 
 import './amplitude';
 
