d89b100b51ddeb43758c9b8f6b0887b66c464e2a
Quang Lam <quang.lam2807@gmail.com>
2020-11-13 12:03:21 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-// to be removed with Electron 11
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
