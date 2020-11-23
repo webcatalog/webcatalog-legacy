d8922de048cabe8859069526fc356aafadde00d9
Quang Lam <quang.lam2807@gmail.com>
2020-11-24 02:16:50 +0700

    Fix ICNS icon is corrupted during installation

--- public/libs/app-management/install-app-async/forked-script-electron-v2.js
+++ public/libs/app-management/install-app-async/forked-script-electron-v2.js
@@ -24,6 +24,7 @@ const Jimp = process.env.NODE_ENV === 'production' ? require('jimp').default : r
 const isUrl = require('is-url');
 const sudo = require('sudo-prompt');
 const ProxyAgent = require('proxy-agent');
+const hasha = require('hasha');
 
 const execAsync = require('../../exec-async');
 const downloadAsync = require('../../download-async');
@@ -318,6 +319,20 @@ Promise.resolve()
       : path.join(dotTemplatePath, 'resources');
     const outputAppAsarUnpackedPath = path.join(resourcesPath, 'app.asar.unpacked');
     return fsExtra.copy(appAsarUnpackedPath, outputAppAsarUnpackedPath, { overwrite: true });
+  })
+  .then(() => {
+    // for unknown reason, on some Macs (electron-package only copies the icon)
+    // electron.icns is corrupted after copying by electron-packager
+    // validate to be sure it's not corrupted
+    // see https://github.com/webcatalog/webcatalog-app/issues/1185
+    // see https://github.com/electron/electron-packager/issues/691
+    if (process.platform === 'darwin') {
+      const destIcnsPath = path.join(dotTemplatePath, 'Contents', 'Resources', 'electron.icns');
+      if (hasha.fromFileSync(destIcnsPath) !== hasha.fromFileSync(iconIcnsPath)) {
+        return Promise.reject(new Error('electron.icns is corrupted when copying.'));
+      }
+    }
+    return null;
   })
   .then(async () => {
     // eslint-disable-next-line no-console
