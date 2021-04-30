e71fc484f7a36c49938d36a4645780fc4c7b9447
Quang Lam <quang.lam2807@gmail.com>
2021-04-30 08:48:47 +0700

    Update dist.js

--- dist.js
+++ dist.js
@@ -34,6 +34,42 @@ const verifyNotarizationAsync = (filePath) => new Promise((resolve, reject) => {
   });
 });
 
+// run each signing task at once
+let codeSigningPromise = Promise.resolve();
+const hsmCodeSignAsync = (filePath) => {
+  codeSigningPromise = codeSigningPromise
+    .then(() => new Promise((resolve, reject) => {
+      const {
+        AZURE_KEY_VAULT_CLIENT_ID,
+        AZURE_KEY_VAULT_CLIENT_SECRET,
+        AZURE_KEY_VAULT_URI,
+        AZURE_KEY_VAULT_CERT_NAME,
+      } = process.env;
+
+      console.log('Signing', filePath);
+      const command = `azuresigntool sign -kvu ${AZURE_KEY_VAULT_URI} -kvc ${AZURE_KEY_VAULT_CERT_NAME} -kvi ${AZURE_KEY_VAULT_CLIENT_ID} -kvs ${AZURE_KEY_VAULT_CLIENT_SECRET} -tr http://rfc3161timestamp.globalsign.com/advanced -td sha256 '${configuration.path}'`;
+      exec(command, { shell: 'powershell.exe' }, (e, stdout, stderr) => {
+        if (e instanceof Error) {
+          console.log(stdout);
+          reject(e);
+          return;
+        }
+
+        if (stderr) {
+          reject(new Error(stderr));
+          return;
+        }
+
+        if (stdout.indexOf('Signing completed successfully') > -1) {
+          resolve(stdout);
+        } else {
+          reject(new Error(stdout));
+        }
+      });
+    }));
+  return codeSigningPromise;
+};
+
 const arch = process.env.TEMPLATE_ARCH || 'x64';
 
 if ((['x64', 'arm64'].indexOf(arch) < 0)) {
@@ -96,35 +132,7 @@ const opts = {
     win: {
       forceCodeSigning: true,
       // https://www.electron.build/configuration/win.html#how-do-delegate-code-signing
-      sign: (configuration) => new Promise((resolve, reject) => {
-        const {
-          AZURE_KEY_VAULT_CLIENT_ID,
-          AZURE_KEY_VAULT_CLIENT_SECRET,
-          AZURE_KEY_VAULT_URI,
-          AZURE_KEY_VAULT_CERT_NAME,
-        } = process.env;
-
-        console.log('Signing', configuration.path);
-        const command = `azuresigntool sign -kvu ${AZURE_KEY_VAULT_URI} -kvc ${AZURE_KEY_VAULT_CERT_NAME} -kvi ${AZURE_KEY_VAULT_CLIENT_ID} -kvs ${AZURE_KEY_VAULT_CLIENT_SECRET} -tr http://rfc3161timestamp.globalsign.com/advanced -td sha256 '${configuration.path}'`;
-        exec(command, { shell: 'powershell.exe' }, (e, stdout, stderr) => {
-          if (e instanceof Error) {
-            console.log(stdout);
-            reject(e);
-            return;
-          }
-
-          if (stderr) {
-            reject(new Error(stderr));
-            return;
-          }
-
-          if (stdout.indexOf('Signing completed successfully') > -1) {
-            resolve(stdout);
-          } else {
-            reject(new Error(stdout));
-          }
-        });
-      }),
+      sign: (configuration) => hsmCodeSignAsync(configuration.path),
     },
     mac: {
       category: 'public.app-category.utilities',
