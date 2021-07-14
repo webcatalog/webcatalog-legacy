a31cc2fedddfe8708d16cb211bd7977050884fe6
Quang Lam <quang.lam2807@gmail.com>
2021-07-14 16:06:42 +0700

    [WEB-60] Fix WebCatalog Accounts authentication-related bugs (#1465)

--- src/components/pages/preferences/section-account.js
+++ src/components/pages/preferences/section-account.js
@@ -16,6 +16,7 @@ import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 
 import {
   requestOpenInBrowser,
+  requestSignInWithPopup,
 } from '../../../senders';
 
 import { open as openDialogLicenseRegistration } from '../../../state/dialog-license-registration/actions';
@@ -50,7 +51,19 @@ const SectionAccount = ({
               <ListItemText primary={registered ? 'WebCatalog Lifetime' : 'WebCatalog Basic'} />
             </ListItem>
             <Divider />
-            <ListItem button onClick={() => requestOpenInBrowser('https://accounts.webcatalog.app/token')}>
+            <ListItem
+              button
+              onClick={() => {
+                // we don't use logging in with protocol webcatalog://
+                // as it causes wrong Electron instance to be opened
+                // e.g. it opens production app instead of dev env
+                if (window.process.platform === 'linux' || process.env.NODE_ENV !== 'production') {
+                  requestSignInWithPopup();
+                  return;
+                }
+                requestOpenInBrowser('https://accounts.webcatalog.app/token');
+              }}
+            >
               <ListItemText primary="Sign in to WebCatalog" />
               <ChevronRightIcon color="action" />
             </ListItem>
