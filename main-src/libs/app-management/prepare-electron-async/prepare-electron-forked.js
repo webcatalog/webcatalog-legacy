9466eafa5e7bf1fe7fb43a0751e18b7d33fe4aab
Quang Lam <quang.lam2807@gmail.com>
2021-11-05 18:10:22 -0500

    Add support for castlabs / electron-releases@16.x

--- main-src/libs/app-management/prepare-electron-async/prepare-electron-forked.js
+++ main-src/libs/app-management/prepare-electron-async/prepare-electron-forked.js
@@ -27,7 +27,7 @@ const path = require('path');
 const { downloadArtifact } = require('@electron/get/dist/cjs');
 
 const formatBytes = require('../../format-bytes');
-const getWvvmpElectronVersion = require('../../get-wvvmp-electron-version');
+const getCastlabsElectronVersion = require('../../get-wvvmp-electron-version');
 
 const argv = yargsParser(process.argv.slice(1));
 const {
@@ -73,7 +73,7 @@ Promise.resolve()
 
     // support widevine cdm on mac or linux x64
     if (process.platform === 'darwin' || (process.platform === 'linux' && process.arch === 'x64')) {
-      downloadOpts.version = getWvvmpElectronVersion(electronVersion);
+      downloadOpts.version = getCastlabsElectronVersion(electronVersion);
       downloadOpts.mirrorOptions = {
         mirror: 'https://github.com/castlabs/electron-releases/releases/download/',
       };
