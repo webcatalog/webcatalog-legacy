f4ba9a78e095a4a9b82e16cabf32e186a0fdba52
Quang Lam <quang.lam2807@gmail.com>
2021-11-23 19:36:56 -0600

    Add option to set spaces or settings page as startup page

--- src/state/router/reducers.js
+++ src/state/router/reducers.js
@@ -3,14 +3,36 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import { combineReducers } from 'redux';
 
-import { ROUTE_HOME, ROUTE_INSTALLED } from '../../constants/routes';
+import {
+  ROUTE_HOME,
+  ROUTE_INSTALLED,
+  ROUTE_SPACES,
+  ROUTE_PREFERENCES,
+} from '../../constants/routes';
 
 import { CHANGE_ROUTE } from '../../constants/actions';
 
 import { getPreference } from '../../senders';
 
-const defaultRoute = window.mode === 'menubar' || getPreference('defaultHome') === 'installed'
-  ? ROUTE_INSTALLED : ROUTE_HOME;
+let defaultRoute;
+switch (getPreference('defaultHome')) {
+  case 'spaces': {
+    defaultRoute = ROUTE_SPACES;
+    break;
+  }
+  case 'installed': {
+    defaultRoute = ROUTE_INSTALLED;
+    break;
+  }
+  case 'preferences': {
+    defaultRoute = ROUTE_PREFERENCES;
+    break;
+  }
+  default: {
+    defaultRoute = ROUTE_HOME;
+  }
+}
+
 const route = (state = defaultRoute, action) => {
   switch (action.type) {
     case CHANGE_ROUTE: return action.route;
