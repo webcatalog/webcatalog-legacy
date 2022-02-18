3991cbb0bfccea9d90505bd1cb5fe23b16f93dbe
Quang Lam <quang.lam2807@gmail.com>
2022-02-17 23:04:49 -0600

    [CU-2a0ykrf] Fix app opening functionality doesn't work on Linux (#1633)

--- main-src/libs/app-management/open-app.js
+++ main-src/libs/app-management/open-app.js
@@ -1,19 +1,30 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-const { app, shell } = require('electron');
+const { app, shell, dialog } = require('electron');
 const path = require('path');
-const { exec } = require('child_process');
 const fsExtra = require('fs-extra');
 
 const { getPreference } = require('../preferences');
+const mainWindow = require('../windows/main');
 
 const openApp = (id, name) => {
   if (process.platform === 'darwin') {
     const appPath = path.join(getPreference('installationPath').replace('~', app.getPath('home')), `${name}.app`);
     shell.openPath(appPath);
   } else if (process.platform === 'linux') {
-    exec(`gtk-launch webcatalog-${id}`);
+    // for unknown reason, we can't launch Linux Electron binary with child_process
+    // regardless of launching the binary directly or through gtk-launch
+    // exec(`gtk-launch webcatalog-${id}`);
+    // so we show user how to open the app manually
+    // can open the binary manually
+    dialog.showMessageBox(mainWindow.get(), {
+      type: 'info',
+      message: `Please launch "${name}" manually using system app launcher.`,
+      buttons: ['OK'],
+      cancelId: 0,
+      defaultId: 0,
+    }).catch(console.log); // eslint-disable-line
   } else if (process.platform === 'win32') {
     const shortcutPath = path.join(getPreference('installationPath'), name, `${name}.lnk`);
     if (fsExtra.existsSync(shortcutPath)) {
