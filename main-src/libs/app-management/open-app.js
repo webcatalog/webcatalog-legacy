3c7138326ba4d5ba40a95dd64dbf4a77506fbe24
Quang Lam <quang.lam2807@gmail.com>
2022-02-17 22:52:07 -0600

    Update open-app.js

--- main-src/libs/app-management/open-app.js
+++ main-src/libs/app-management/open-app.js
@@ -1,11 +1,12 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-const { app, shell } = require('electron');
+const { app, shell, dialog } = require('electron');
 const path = require('path');
 const fsExtra = require('fs-extra');
 
 const { getPreference } = require('../preferences');
+const mainWindow = require('../windows/main');
 
 const openApp = (id, name) => {
   if (process.platform === 'darwin') {
@@ -15,10 +16,15 @@ const openApp = (id, name) => {
     // for unknown reason, we can't launch Linux Electron binary with child_process
     // regardless of launching the binary directly or through gtk-launch
     // exec(`gtk-launch webcatalog-${id}`);
-    // so we will show the binary file in file manager instead, so user
+    // so we show user how to open the app manually
     // can open the binary manually
-    const binPath = path.join(getPreference('installationPath').replace('~', app.getPath('home')), name, name);
-    shell.showItemInFolder(binPath);
+    dialog.showMessageBox(mainWindow.get(), {
+      type: 'info',
+      message: `Please launch "${name}" manually using system app launcher.`,
+      buttons: ['OK'],
+      cancelId: 0,
+      defaultId: 0,
+    }).catch(console.log); // eslint-disable-line
   } else if (process.platform === 'win32') {
     const shortcutPath = path.join(getPreference('installationPath'), name, `${name}.lnk`);
     if (fsExtra.existsSync(shortcutPath)) {
