aaad9ac64c3b8b09b53eeb002a7e5f95a87110f9
Quang Lam <quang.lam2807@gmail.com>
2022-03-17 23:05:27 -0500

    fix

--- src/components/root/sidebar/upgrade-button.js
+++ src/components/root/sidebar/upgrade-button.js
@@ -3,31 +3,30 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 /* eslint-disable no-constant-condition */
 import React from 'react';
-import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Button from '@material-ui/core/Button';
 
 import { open as openDialogLicenseRegistration } from '../../../state/dialog-license-registration/actions';
 
-import connectComponent from '../../../helpers/connect-component';
-
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   container: {
     paddingBottom: theme.spacing(1),
     display: 'flex',
     justifyContent: 'center',
   },
-});
+}));
 
-const UpgradeButton = ({
-  classes,
-  registered,
-  onOpenDialogLicenseRegistration,
-}) => {
+const UpgradeButton = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const registered = useSelector((state) => state.preferences.registered);
+
   if (!registered) {
     return (
       <div className={classes.container}>
-        <Button size="small" onClick={() => onOpenDialogLicenseRegistration()} color="inherit">
+        <Button size="small" onClick={() => dispatch(openDialogLicenseRegistration())} color="inherit">
           Upgrade
         </Button>
       </div>
@@ -37,27 +36,4 @@ const UpgradeButton = ({
   return null;
 };
 
-UpgradeButton.defaultProps = {
-  registered: false,
-};
-
-UpgradeButton.propTypes = {
-  classes: PropTypes.object.isRequired,
-  onOpenDialogLicenseRegistration: PropTypes.func.isRequired,
-  registered: PropTypes.bool,
-};
-
-const mapStateToProps = (state) => ({
-  registered: state.preferences.registered,
-});
-
-const actionCreators = {
-  openDialogLicenseRegistration,
-};
-
-export default connectComponent(
-  UpgradeButton,
-  mapStateToProps,
-  actionCreators,
-  styles,
-);
+export default UpgradeButton;
